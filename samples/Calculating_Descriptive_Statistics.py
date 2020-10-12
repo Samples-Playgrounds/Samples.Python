016553d41e456224ac88f6ba9e07c7ba4d00c592
katodix <darko.katovic@gmail.com>
2020-10-12 18:13:28 +0200

    more

--- samples/Calculating_Descriptive_Statistics.py
+++ samples/Calculating_Descriptive_Statistics.py
@@ -3,13 +3,18 @@
 
 # https://realpython.com/python-statistics/
 
-import math
-import statistics
-import numpy as np
-import scipy.stats
+#import math
+#import statistics
+#import numpy as np
+#import scipy.stats
 import pandas as pd
 
 data = pd.read_csv("/Users/katodix/Projects/HolisticWare.Core.Math.Statistics.Descriptive.Sequential/externals/Core.Math.Samples/data/Pejcic_318.csv")
 
 data['ATV'].mean()
 data.mean()
+
+
+statistics.mean(data['ATV'])
+statistics.harmonic_mean(data['ATV'])
+statistics.geometric_mean(data['ATV'])
