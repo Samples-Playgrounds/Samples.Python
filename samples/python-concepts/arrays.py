2df33821f23c7130f6167c825df51690a78d8b66
moljac <mcvjetko@holisticware.net>
2021-08-18 15:46:07 +0300

    ärrays added

---
+++ samples/python-concepts/arrays.py
@@ -0,0 +1,12 @@
+
+integers = [ 1, 6, 4, 3, 15, -2]
+
+print("integers[0] = ", integers[0])
+print("integers[1] = ", integers[1])
+print("integers[2] = ", integers[2])
+print("integers[3] = ", integers[3])
+print("integers[4] = ", integers[4])
+
+
+
+print("integers[5] = ", integers[5])
