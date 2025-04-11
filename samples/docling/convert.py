04d7ed31449a53d69168b28a59307fe469e2c742
moljac <mcvjetko@holisticware.net>
2025-04-11 13:17:40 +0200

    refactor sample to method

--- samples/docling/convert.py
+++ samples/docling/convert.py
@@ -4,6 +4,7 @@
 # pip3.13 freeze > requirements.txt
 # pip3.13 install -r requirements.txt
 # python3.13 convert.py
+
 from docling.document_converter import DocumentConverter
 
 # document per local path or URL
@@ -11,6 +12,18 @@ source = "/Volumes/FAT_VERB/learning/topics/security/threat-modelling/effectivet
 # "https://arxiv.org/pdf/2408.09869"  
 # /Volumes/FAT_VERB/learning/topics/security/threat-modelling/threatmodeling.pdf
 
-converter = DocumentConverter()
-result = converter.convert(source)
-print(result.document.export_to_markdown())  # output: "## Docling Technical Report[...]"
+def covert_docling(source: str) -> str:
+    converter = DocumentConverter()
+    print(f"Docling load {source}")
+    result = converter.convert(source)
+    resuld_md = result.document.export_to_markdown()  # output: "## Docling Technical Report[...]"
+    print(resuld_md)
+    # save to file
+    print(f"Docling save {source}.md")
+    with open(f"{source}.md", "w") as f:
+        f.write(resuld_md)
+
+    return resuld_md
+
+
+covert_docling(source)
