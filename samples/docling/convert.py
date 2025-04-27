5c3cade90135b68ed752d269b399d360af57c65d
moljac <mcvjetko@holisticware.net>
2025-04-27 13:25:03 +0200

    fixes

--- samples/docling/convert.py
+++ samples/docling/convert.py
@@ -14,16 +14,16 @@ source = "/Volumes/FAT_VERB/learning/topics/security/threat-modelling/effectivet
 
 def covert_docling(source: str) -> str:
     converter = DocumentConverter()
-    print(f"Docling load {source}")
+    print(f"python: docling.document_converter load {source}")
     result = converter.convert(source)
     resuld_md = result.document.export_to_markdown()  # output: "## Docling Technical Report[...]"
     print(resuld_md)
     # save to file
-    print(f"Docling save {source}.md")
-    with open(f"{source}.md", "w") as f:
-        f.write(resuld_md)
+    print(f"python: docling.document_converter save {source}.docling.md")
+    with open(f"{source}.docling.md", "w") as f:
+        f.write(result_md) 
 
-    return resuld_md
+    return result_md
 
 
 covert_docling(source)
