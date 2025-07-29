d724dc269f367186287c1514f0bbbd3878ba1d49
moljac <mcvjetko@holisticware.net>
2025-07-29 08:59:13 +0200

    markitdown for new data

--- samples/etl-extract-transform-load/extractions/document/markitdown/main.py
+++ samples/etl-extract-transform-load/extractions/document/markitdown/main.py
@@ -8,27 +8,31 @@
 import api
 
 # document per local path or URL
-source = "/Volumes/xFAT-1TB-2/learning/books/topics/sports/Moljac_Knjiga/RUKOPIS_ver_Final.pdf"
-#source = "/Volumes/FAT_VERB/learning/books/topics/microsoft/architecture/adevelopersguidetonetinazure.pdf"
-#source = "https://arxiv.org/pdf/2408.09869"  
+sources = [
+   "../../../../../data/pravno/zakoni/zakon_o_sportskoj_inspekciji_nn_86_12.pdf",
+   "../../../../../data/pravno/zakoni/kazneni-zakon.docx",
+   "../../../../../data/pravno/zakoni/zakon-o-sportu-NN-19-16.pdf",
+   "../../../../../data/pravno/zakoni/zakon-o-sportu-2022.pdf",
+   "../../../../../data/pravno/zakoni/Kazneni-zakon.pdf",
+   "../../../../../data/pravno/zakoni/55-4.pdf",
+   "../../../../../data/pravno/satuti/hjs/status-HJS-a-2024.pdf",
+   "../../../../../data/pravno/satuti/hoo/Statut-HOO-a_procisceni-tekst_srpanj-2024.pdf",
+#  "/Volumes/xFAT-1TB-2/learning/books/topics/sports/Moljac_Knjiga/RUKOPIS_ver_Final.pdf",
+#  "/Volumes/xFAT-1TB-2/learning/books/topics/microsoft/dotnet/maui/NET_MAUI_in_Action.pdf",
+#  "/Volumes/xFAT-1TB-2/learning/books/topics/microsoft/dotnet/maui/mvvmpatterninnetmaui.pdf",
+#  "/Volumes/xFAT-1TB-2/learning/books/topics/sports/Moljac_Knjiga/doc_files/13.\ Funkcionalna\ procjena\ pokreta.docx",
+#  "/Volumes/xFAT-1TB-2/learning/books/topics/sports/Moljac_Knjiga/doc_files/14.\ Literatura.docx",
+]
+#  "/Volumes/FAT_VERB/learning/books/topics/microsoft/architecture/adevelopersguidetonetinazure.pdf"
+#   "https://arxiv.org/pdf/2408.09869"
 
-result_md = api.covert_file_to_markdown(source)
-print(result_md)
 
-# save to file
-print(f"python: docling.document_converter save {source}.markitdown_pdf.md")
-with open(f"{source}.markitdown_pdf.md", "w") as f:
-   f.write(result_md) 
-
-source = "/Volumes/xFAT-1TB-2/learning/books/topics/sports/Moljac_Knjiga/doc_files/0. Naslovnica.docx"
-
-result_md = api.covert_file_to_markdown(source)
-print(result_md)
-
-
-# save to file
-print(f"python: docling.document_converter save {source}.markitdown_docx.md")
-with open(f"{source}.markitdown_docx.md", "w") as f:
-   f.write(result_md) 
+for source in sources:
+   if source.endswith(".pdf"):
+      result_md = api.covert_file_to_markdown(source)
+      print(result_md)
+      print(f"python: markitdown.document_converter save {source}.markitdown.md")
+      with open(f"{source}.markitdown.md", "w") as f:
+         f.write(result_md) 
 
 
