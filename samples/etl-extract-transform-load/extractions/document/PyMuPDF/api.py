56ad9d396bc8dadd71a060ddd171e946a925e8be
moljac <mcvjetko@holisticware.net>
2025-07-31 06:57:33 +0200

    Update api.py

--- samples/etl-extract-transform-load/extractions/document/PyMuPDF/api.py
+++ samples/etl-extract-transform-load/extractions/document/PyMuPDF/api.py
@@ -1,5 +1,45 @@
 import fitz  # PyMuPDF
 
+# https://pymupdf.readthedocs.io/en/latest/how-to-open-a-file.html#supported-file-types
+# Document Formats
+# 
+#     PDF 
+#     
+#     XPS 
+#     EPUB 
+#     MOBI 
+#     FB2 
+#     CBZ 
+#     SVG 
+#     TXT
+# 
+# Image Formats
+# Input formats
+#     JPG/JPEG,
+#     PNG,
+#     BMP,
+#     GIF,
+#     TIFF,
+#     PNM,
+#     PGM,
+#     PBM,
+#     PPM,
+#     PAM,
+#     JXR,
+#     JPX/JP2,
+#     PSD
+# Output formats
+#     JPG/JPEG,
+#     PNG,
+#     PNM,
+#     PGM,
+#     PBM,
+#     PPM,
+#     PAM,
+#     PSD,
+#     PS
+
+
 def covert_pdf_file_to_text (source: str) -> str:
     doc = fitz.open(source)
     result_txt = ""
