6ff5906d7f78d469fd0871b4ac15fd1b60a314f7
moljac <mcvjetko@holisticware.net>
2025-07-31 06:57:36 +0200

    Update main.py

--- samples/etl-extract-transform-load/extractions/document/PyMuPDF/main.py
+++ samples/etl-extract-transform-load/extractions/document/PyMuPDF/main.py
@@ -1,19 +1,30 @@
 import api
 
 # document per local path or URL
-# source = "/Volumes/FAT_VERB/learning/topics/security/threat-modelling/effectivethreatinvestigationforsocanalysts.pdf"
-# "https://arxiv.org/pdf/2408.09869"  
-# document per local path or URL
-source = "/Volumes/xFAT-1TB-2/learning/books/topics/sports/Moljac_Knjiga/RUKOPIS_ver_Final.pdf"
-#source = "/Volumes/FAT_VERB/learning/books/topics/microsoft/architecture/adevelopersguidetonetinazure.pdf"
-#source = "/Volumes/FAT_VERB/learning/topics/security/threat-modelling/threatmodeling.pdf
-#source = "https://arxiv.org/pdf/2408.09869"  
+sources = [
+   "../../../../../data/pravno/zakoni/zakon_o_sportskoj_inspekciji_nn_86_12.pdf",
+   "../../../../../data/pravno/zakoni/kazneni-zakon.docx",
+   "../../../../../data/pravno/zakoni/zakon-o-sportu-NN-19-16.pdf",
+   "../../../../../data/pravno/zakoni/zakon-o-sportu-2022.pdf",
+   "../../../../../data/pravno/zakoni/Kazneni-zakon.pdf",
+   "../../../../../data/pravno/zakoni/55-4.pdf",
+   "../../../../../data/pravno/satuti/hjs/status-HJS-a-2024.pdf",
+   "../../../../../data/pravno/satuti/hoo/Statut-HOO-a_procisceni-tekst_srpanj-2024.pdf",
+#  "/Volumes/xFAT-1TB-2/learning/books/topics/sports/Moljac_Knjiga/RUKOPIS_ver_Final.pdf",
+#  "/Volumes/xFAT-1TB-2/learning/books/topics/microsoft/dotnet/maui/NET_MAUI_in_Action.pdf",
+#  "/Volumes/xFAT-1TB-2/learning/books/topics/microsoft/dotnet/maui/mvvmpatterninnetmaui.pdf",
+#  "/Volumes/xFAT-1TB-2/learning/books/topics/sports/Moljac_Knjiga/doc_files/13.\ Funkcionalna\ procjena\ pokreta.docx",
+#  "/Volumes/xFAT-1TB-2/learning/books/topics/sports/Moljac_Knjiga/doc_files/14.\ Literatura.docx",
+]
+#  "/Volumes/FAT_VERB/learning/books/topics/microsoft/architecture/adevelopersguidetonetinazure.pdf"
+#   "https://arxiv.org/pdf/2408.09869"
 
-result_txt = api.covert_pdf_file_to_text(source)
+for source in sources:
+   if source.endswith(".pdf"):
+      result_txt = api.covert_pdf_file_to_text(source)
+      print(result_txt)
 
-print(result_txt)
-
-# save to file
-print(f"python: docling.document_converter save {source}.PyMuPDF-fitz.txt")
-with open(f"{source}.PyMuPDF-fitz.txt", "w") as f:
-   f.write(result_txt) 
+      # save to file
+      print(f"python: docling.document_converter save {source}.PyMuPDF-fitz.txt")
+      with open(f"{source}.PyMuPDF-fitz.txt", "w") as f:
+         f.write(result_txt) 
