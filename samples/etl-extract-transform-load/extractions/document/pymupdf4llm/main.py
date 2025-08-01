9f28bcbad95b08c7e9abd1aa9b9c097c49eb37b6
moljac <mcvjetko@holisticware.net>
2025-08-01 09:27:40 +0200

    pymupdf4llm

--- samples/etl-extract-transform-load/extractions/document/pymupdf4llm/main.py
+++ samples/etl-extract-transform-load/extractions/document/pymupdf4llm/main.py
@@ -7,24 +7,40 @@
 
 import api
 
+import api
+
 # document per local path or URL
-source = "/Volumes/xFAT-1TB-2/learning/books/topics/sports/Moljac_Knjiga/RUKOPIS_ver_Final.pdf"
-#source = "/Volumes/FAT_VERB/learning/books/topics/microsoft/architecture/adevelopersguidetonetinazure.pdf"
-#source = "https://arxiv.org/pdf/2408.09869"  
+sources = [
+   "../../../../../data/pravno/zakoni/zakon_o_sportskoj_inspekciji_nn_86_12.pdf",
+   "../../../../../data/pravno/zakoni/kazneni-zakon.docx",
+   "../../../../../data/pravno/zakoni/zakon-o-sportu-NN-19-16.pdf",
+   "../../../../../data/pravno/zakoni/zakon-o-sportu-2022.pdf",
+   "../../../../../data/pravno/zakoni/Kazneni-zakon.pdf",
+   "../../../../../data/pravno/zakoni/55-4.pdf",
+   "../../../../../data/pravno/satuti/hjs/status-HJS-a-2024.pdf",
+   "../../../../../data/pravno/satuti/hoo/Statut-HOO-a_procisceni-tekst_srpanj-2024.pdf",
+#  "/Volumes/xFAT-1TB-2/learning/books/topics/sports/Moljac_Knjiga/RUKOPIS_ver_Final.pdf",
+#  "/Volumes/xFAT-1TB-2/learning/books/topics/microsoft/dotnet/maui/NET_MAUI_in_Action.pdf",
+#  "/Volumes/xFAT-1TB-2/learning/books/topics/microsoft/dotnet/maui/mvvmpatterninnetmaui.pdf",
+#  "/Volumes/xFAT-1TB-2/learning/books/topics/sports/Moljac_Knjiga/doc_files/13.\ Funkcionalna\ procjena\ pokreta.docx",
+#  "/Volumes/xFAT-1TB-2/learning/books/topics/sports/Moljac_Knjiga/doc_files/14.\ Literatura.docx",
+]
+#  "/Volumes/FAT_VERB/learning/books/topics/microsoft/architecture/adevelopersguidetonetinazure.pdf"
+#   "https://arxiv.org/pdf/2408.09869"
 
-result_md = api.covert_pdf_file_to_markdown(source)
+for source in sources:
 
-print(result_md)
+   if source.endswith(".pdf"):
+      result_md = api.covert_pdf_file_to_markdown(source)
+      print(result_md)
 
-# save to file
-print(f"python: docling.document_converter save {source}.pymupdf4llm.1.md")
-with open(f"{source}.pymupdf4llm.1.md", "w") as f:
-   f.write(result_md) 
-
-
-# now work with the markdown text, e.g. store as a UTF8-encoded file
-import pathlib
-print(f"python: docling.document_converter save {source}.pymupdf4llm.2.md")
-pathlib.Path(f"{source}.pymupdf4llm.2.md").write_bytes(result_md.encode())
+      # save to file
+      print(f"python: pymupdf4llm save {source}.pymupdf4llm.1.md")
+      with open(f"{source}.pymupdf4llm.1.md", "w") as f:
+         f.write(result_md) 
 
 
+      # now work with the markdown text, e.g. store as a UTF8-encoded file
+      import pathlib
+      print(f"python: pymupdf4llm save {source}.pymupdf4llm.2.md")
+      pathlib.Path(f"{source}.pymupdf4llm.2.md").write_bytes(result_md.encode())
