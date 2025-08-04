abcd6c776e081e72b9785d82a1f280adfa74a8f7
moljac <mcvjetko@holisticware.net>
2025-08-04 23:40:11 +0200

    refactoring

--- samples/etl-extract-transform-load/extractions/document/unstructured/main.py
+++ samples/etl-extract-transform-load/extractions/document/unstructured/main.py
@@ -8,23 +8,36 @@
 import api
 
 # document per local path or URL
-source = "/Volumes/xFAT-1TB-2/learning/books/topics/sports/Moljac_Knjiga/RUKOPIS_ver_Final.pdf"
-#source = "/Volumes/FAT_VERB/learning/books/topics/microsoft/architecture/adevelopersguidetonetinazure.pdf"
-#source = "https://arxiv.org/pdf/2408.09869"  
+sources = [
+   "../../../../../data/pravno/zakoni/zakon_o_sportskoj_inspekciji_nn_86_12.pdf",
+   "../../../../../data/pravno/zakoni/kazneni-zakon.docx",
+   "../../../../../data/pravno/zakoni/zakon-o-sportu-NN-19-16.pdf",
+   "../../../../../data/pravno/zakoni/zakon-o-sportu-2022.pdf",
+   "../../../../../data/pravno/zakoni/Kazneni-zakon.pdf",
+   "../../../../../data/pravno/zakoni/55-4.pdf",
+   "../../../../../data/pravno/satuti/hjs/status-HJS-a-2024.pdf",
+   "../../../../../data/pravno/satuti/hoo/Statut-HOO-a_procisceni-tekst_srpanj-2024.pdf",
+#  "/Volumes/xFAT-1TB-2/learning/books/topics/sports/Moljac_Knjiga/RUKOPIS_ver_Final.pdf",
+#  "/Volumes/xFAT-1TB-2/learning/books/topics/microsoft/dotnet/maui/NET_MAUI_in_Action.pdf",
+#  "/Volumes/xFAT-1TB-2/learning/books/topics/microsoft/dotnet/maui/mvvmpatterninnetmaui.pdf",
+#  "/Volumes/xFAT-1TB-2/learning/books/topics/sports/Moljac_Knjiga/doc_files/13.\ Funkcionalna\ procjena\ pokreta.docx",
+#  "/Volumes/xFAT-1TB-2/learning/books/topics/sports/Moljac_Knjiga/doc_files/14.\ Literatura.docx",
+]
+#  "/Volumes/FAT_VERB/learning/books/topics/microsoft/architecture/adevelopersguidetonetinazure.pdf"
+#   "https://arxiv.org/pdf/2408.09869"
 
-result_txt = api.covert_pdf_file_to_text(source)
 
-print(result_txt)
+for source in sources:
+   if source.endswith(".pdf"):
 
-# save to file
-print(f"python: unstructured save {source}.unstructured.1.md")
-with open(f"{source}.unstructured.1.md", "w") as f:
-   f.write(result_txt) 
+      result_txt = api.covert_pdf_file_to_text(source)
+      print(result_txt)
+
+      # save to file
+      print(f"python: unstructured save {source}.unstructured.1.txt")
+      with open(f"{source}.unstructured.1.txt", "w") as f:
+         f.write(result_txt) 
 
 
-# now work with the markdown text, e.g. store as a UTF8-encoded file
-import pathlib
-print(f"python: unstructured save {source}.unstructured.2.md")
-pathlib.Path(f"{source}.unstructured.2.md").write_bytes(result_txt.encode())
 
 
