b92457b4d2fff6a30f6fce3b309beda5658f673c
moljac <mcvjetko@holisticware.net>
2025-08-07 19:07:48 +0200

    Update main.py

--- samples/etl-extract-transform-load/splitting-chunking/llama_index.text_splitter-SentenceSplitter/main.py
+++ samples/etl-extract-transform-load/splitting-chunking/llama_index.text_splitter-SentenceSplitter/main.py
@@ -1,16 +1,24 @@
+# python3.13 -m venv .venv
+# source .venv/bin/activate
+# pip3.13 install "llama_index"
+# pip3.13 freeze > requirements.txt
 
 import api
 
 # document per local path or URL
-texts = [
-   "This is the text I would like to chunk up. It is the example text for this exercise",
+file="mit.txt"
 
-]
 #  "/Volumes/FAT_VERB/learning/books/topics/microsoft/architecture/adevelopersguidetonetinazure.pdf"
 #   "https://arxiv.org/pdf/2408.09869"
 
+from llama_index.readers.file import FlatReader
+from pathlib import Path
 
-for text in texts:
-      chunks = api.split_on_character(text, 35)
-      for i, chunk in enumerate(chunks):
-          print(f"Chunk {i}: {chunk}")
+md_docs = FlatReader().load_data(Path(file))
+
+nodes = api.split_text(md_docs, 2000, 20)
+
+for node in nodes:
+   print(f"============================================================================")
+   print(node)
+
