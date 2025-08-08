9d33c240687270ab238c130a486f2dac96f626d9
moljac <mcvjetko@holisticware.net>
2025-08-08 14:08:24 +0200

    Create main.py

---
+++ samples/etl-extract-transform-load/splitting-chunking/langchain_ollama.OllamaLLM/main.py
@@ -0,0 +1,35 @@
+# python3.13 -m venv .venv
+# source .venv/bin/activate
+# pip3.13 install "llama_index"
+# pip3.13 freeze > requirements.txt
+
+# import api
+
+# document per local path or URL
+#file="mit.txt"
+
+#  "/Volumes/FAT_VERB/learning/books/topics/microsoft/architecture/adevelopersguidetonetinazure.pdf"
+#   "https://arxiv.org/pdf/2408.09869"
+
+print(f"============================================================================")
+from langchain_ollama import OllamaLLM
+
+llm = OllamaLLM(model="llama3.3:latest")
+
+#    raise mapped_exc(message) from exc
+# httpx.ConnectError: [Errno 61] Connection refused
+
+# ollama run llama3.3:latest 
+response = llm.invoke("The first man on the moon was ...")
+
+print(f"response = {response}")
+
+
+print(f"============================================================================")
+from langchain_ollama import ChatOllama
+
+chat_model = ChatOllama(model="llama3.3:latest")
+
+response = chat_model.invoke("Who was the first man on the moon?")
+
+print(f"response = {response}")
