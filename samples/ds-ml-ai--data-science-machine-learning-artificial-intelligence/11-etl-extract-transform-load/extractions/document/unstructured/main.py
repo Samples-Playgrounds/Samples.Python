58e67482d7025dbec665d5617084f167237b8667
moljac <mcvjetko@holisticware.net>
2025-08-18 16:10:10 +0200

    Update main.py

--- samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/document/unstructured/main.py
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/document/unstructured/main.py
@@ -9,19 +9,19 @@ import api
 
 # document per local path or URL
 sources = [
-   "../../../../../data/pravno/zakoni/zakon_o_sportskoj_inspekciji_nn_86_12.pdf",
-   "../../../../../data/pravno/zakoni/kazneni-zakon.docx",
-   "../../../../../data/pravno/zakoni/zakon-o-sportu-NN-19-16.pdf",
-   "../../../../../data/pravno/zakoni/zakon-o-sportu-2022.pdf",
-   "../../../../../data/pravno/zakoni/Kazneni-zakon.pdf",
-   "../../../../../data/pravno/zakoni/55-4.pdf",
-   "../../../../../data/pravno/satuti/hjs/status-HJS-a-2024.pdf",
-   "../../../../../data/pravno/satuti/hoo/Statut-HOO-a_procisceni-tekst_srpanj-2024.pdf",
-   "../../../../../data/pravno/satuti/gnk-dinamo/Nacrt+Prijedloga+Statuta+GNK+Dinamo.pdf",
-   "../../../../../data/pravno/satuti/gnk-dinamo/statut-dinamo.2023.pdf",
-   "../../../../../data/pravno/satuti/gnk-dinamo/",
-   "../../../../../data/pravno/dkoumenti/hjs-zppi-2023-rjesenje.2.600dpi.compressed.pdf",
-   "../../../../../data/pravno/dkoumenti/Rješenje - odbijanje zahtjeva Miljenko CVJETKO - 18.8.2025. .pdf"
+   "../../../../../../data/pravno/zakoni/zakon_o_sportskoj_inspekciji_nn_86_12.pdf",
+   "../../../../../../data/pravno/zakoni/kazneni-zakon.docx",
+   "../../../../../../data/pravno/zakoni/zakon-o-sportu-NN-19-16.pdf",
+   "../../../../../../data/pravno/zakoni/zakon-o-sportu-2022.pdf",
+   "../../../../../../data/pravno/zakoni/Kazneni-zakon.pdf",
+   "../../../../../../data/pravno/zakoni/55-4.pdf",
+   "../../../../../../data/pravno/satuti/hjs/status-HJS-a-2024.pdf",
+   "../../../../../../data/pravno/satuti/hoo/Statut-HOO-a_procisceni-tekst_srpanj-2024.pdf",
+   "../../../../../../data/pravno/satuti/gnk-dinamo/Nacrt+Prijedloga+Statuta+GNK+Dinamo.pdf",
+   "../../../../../../data/pravno/satuti/gnk-dinamo/statut-dinamo.2023.pdf",
+   "../../../../../../data/pravno/satuti/gnk-dinamo/",
+   "../../../../../../data/pravno/dkoumenti/hjs-zppi-2023-rjesenje.2.600dpi.compressed.pdf",
+   "../../../../../../data/pravno/dkoumenti/Rješenje - odbijanje zahtjeva Miljenko CVJETKO - 18.8.2025. .pdf"
 #  "/Volumes/xFAT-1TB-2/learning/books/topics/sports/Moljac_Knjiga/RUKOPIS_ver_Final.pdf",
 #  "/Volumes/xFAT-1TB-2/learning/books/topics/microsoft/dotnet/maui/NET_MAUI_in_Action.pdf",
 #  "/Volumes/xFAT-1TB-2/learning/books/topics/microsoft/dotnet/maui/mvvmpatterninnetmaui.pdf",
