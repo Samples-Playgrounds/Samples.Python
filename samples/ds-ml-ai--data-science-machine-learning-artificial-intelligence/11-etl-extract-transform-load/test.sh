3eb65804a8f85175636b0245405240b7851cc68b
moljac <mcvjetko@holisticware.net>
2025-08-18 15:46:22 +0200

    Update test.sh

--- samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/test.sh
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/test.sh
@@ -2,22 +2,65 @@
 
 
 cd ./extractions/document/docling/
-python main.py
-cd -
-
-cd ./extractions/document/unstrucutred/
+rm -fr .venv
+python3.13 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+pip install --upgrade pip
+pip install -r requirements.txt
 python main.py
 cd -
 
 cd ./extractions/document/markitdown/
+rm -fr .venv
+python3.13 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+pip install --upgrade pip
+pip install -r requirements.txt
 python main.py
 cd -
 
 cd ./extractions/document/PyMuPDF/
+rm -fr .venv
+python3.13 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+pip install --upgrade pip
+pip install -r requirements.txt
+python main.py
+cd -
+
+cd ./extractions/document/PyPDF2/
+rm -fr .venv
+python3.13 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+pip install --upgrade pip
+pip install -r requirements.txt
 python main.py
 cd -
 
 cd ./extractions/document/pymupdf4llm/
+rm -fr .venv
+python3.13 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+pip install --upgrade pip
+pip install -r requirements.txt
 python main.py
 cd -
+
+cd ./extractions/document/unstructured/
+rm -fr .venv
+python3.13 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+pip install --upgrade pip
+pip install -r requirements.txt
+python main.py
+cd -
+
+
+
 
