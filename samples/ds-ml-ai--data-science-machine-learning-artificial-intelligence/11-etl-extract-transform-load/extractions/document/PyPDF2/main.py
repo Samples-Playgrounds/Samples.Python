a247e9d1dd307e78e26c7116488579d8d305eb90
moljac <mcvjetko@holisticware.net>
2025-08-21 08:52:58 +0200

    Update main.py

--- samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/document/PyPDF2/main.py
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/document/PyPDF2/main.py
@@ -13,8 +13,9 @@ sources = [
    "../../../../../../data/pravno/satuti/hjs/status-HJS-a-2024.pdf",
    "../../../../../../data/pravno/satuti/hoo/Statut-HOO-a_procisceni-tekst_srpanj-2024.pdf",
    "../../../../../../data/pravno/satuti/gnk-dinamo/Nacrt+Prijedloga+Statuta+GNK+Dinamo.pdf",
+   "../../../../../../data/pravno/satuti/gnk-dinamo/Prijedlog_Statuta_GNK_Dinamo_finalno_20250811_za+IO.pdf",
    "../../../../../../data/pravno/satuti/gnk-dinamo/statut-dinamo.2023.pdf",
-   "../../../../../../data/pravno/satuti/gnk-dinamo/",
+   "../../../../../../data/pravno/satuti/gnk-dinamo/O Statutu_250814_100408.pdf",
    "../../../../../../data/pravno/dokumenti/hjs-zppi-2023-rjesenje.2.600dpi.compressed.pdf",
    "../../../../../../data/pravno/dokumenti/Rješenje - odbijanje zahtjeva Miljenko CVJETKO - 18.8.2025. .pdf"
 #  "/Volumes/xFAT-1TB-2/learning/books/topics/sports/Moljac_Knjiga/RUKOPIS_ver_Final.pdf",
