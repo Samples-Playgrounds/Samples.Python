dc3588faa7d620da75eb1169679d9ddffbbbd212
moljac <mcvjetko@holisticware.net>
2025-08-29 21:08:39 +0200

    Create api.py

---
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/04-formatters/safetensors/api.py
@@ -0,0 +1,33 @@
+import torch
+import os
+
+from safetensors import safe_open
+from safetensors.torch import save_file
+from typing import Any, Dict, List, Optional, Set, Tuple, Union
+
+
+# https://huggingface.co/docs/safetensors/en/api/torch
+
+def write_all_tensors(
+    filename: str,
+    tensors: Dict[str, torch.Tensor],
+    metadata: Optional[Dict[str, str]] = None,
+) -> str:
+
+    save_file(tensors, filename, metadata=metadata)
+
+    # Optionally, you can return the filename or any other relevant information
+    return filename
+
+def read_all_tensors(
+    filename: str,
+    metadata: Optional[Dict[str, str]] = None,
+) -> Dict[str, torch.Tensor]:
+
+    tensors = {}
+    with safe_open("model.safetensors", framework="pt", device="cpu") as f:
+        for key in f.keys():
+            tensors[key] = f.get_tensor(key)
+    return tensors
+    
+    # return api.load_file(filename, metadata=metadata)
