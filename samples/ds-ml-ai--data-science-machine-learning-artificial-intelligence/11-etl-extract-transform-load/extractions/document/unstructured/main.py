679d25fca94ea31de099d165e9ba0602d9cb6402
moljac <mcvjetko@holisticware.net>
2025-08-30 23:03:00 +0200

    Update main.py

--- samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/document/unstructured/main.py
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/document/unstructured/main.py
@@ -1,9 +1,11 @@
-# python3.13 -m venv .venv
-# source .venv/bin/activate
-# pip3.13 install "unstructured[all-docs]"
-# pip3.13 freeze > requirements.txt
-# pip3.13 install -r requirements.txt
-# python3.13 main.py
+"""
+python3.13 -m venv .venv
+source .venv/bin/activate
+pip3.13 install "unstructured[all-docs]"
+pip3.13 freeze > requirements.txt
+pip3.13 install -r requirements.txt
+python3.13 main.py
+"""
 
 import api
 
