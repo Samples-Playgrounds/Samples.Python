ce17cd572b5e95dcfceddc4a2f7e487b2b910ac4
moljac <mcvjetko@holisticware.net>
2025-09-08 22:01:32 +0200

    Delete api.py

--- samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/document/markitdown/api.py
+++
@@ -1,7 +0,0 @@
-from markitdown import MarkItDown
-
-def covert_file_to_markdown (source: str) -> str:
-    md = MarkItDown(enable_plugins=False) # Set to True to enable plugins
-    result_md = md.convert(source)
-
-    return result_md.text_content
