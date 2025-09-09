ba03c0092e1099871f8ecb06dcb2bc12a716dcca
moljac <mcvjetko@holisticware.net>
2025-09-09 22:00:12 +0200

    Create api_PyPDF2.py

---
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/document/PyPDF2/api_PyPDF2.py
@@ -0,0 +1,17 @@
+try:
+    import Image
+except ImportError:
+    from PIL import Image
+
+import pytesseract
+import PyPDF2
+
+from PyPDF2 import PdfReader
+
+def covert_pdf_file_to_markdown (source: str) -> str:
+    reader = PdfReader(source)
+    result_md = ""
+    for page in reader.pages:
+        result_md = result_md + page.extract_text()
+
+    return result_md
