4c994011db1e7488a51a26c9a9a4f83129bbe01d
moljac <mcvjetko@holisticware.net>
2025-09-18 19:08:57 +0200

    Update test.sh

--- samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/test.sh
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/test.sh
@@ -2,8 +2,8 @@
 
 
 cd ./extractions/document/pdf/docling/
-rm -fr .venv
-python3.13 -m venv .venv
+rm -fr .venv __pycache__ *.pyc
+python -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
 pip install --upgrade pip
@@ -12,8 +12,8 @@ python main.py
 cd -
 
 cd ./extractions/document/pdf/markitdown/
-rm -fr .venv
-python3.13 -m venv .venv
+rm -fr .venv __pycache__ *.pyc
+python -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
 pip install --upgrade pip
@@ -22,8 +22,8 @@ python main.py
 cd -
 
 cd ./extractions/document/pdf/PyMuPDF/
-rm -fr .venv
-python3.13 -m venv .venv
+rm -fr .venv __pycache__ *.pyc
+python -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
 pip install --upgrade pip
@@ -32,8 +32,8 @@ python main.py
 cd -
 
 cd ./extractions/document/pdf/PyPDF2/
-rm -fr .venv
-python3.13 -m venv .venv
+rm -fr .venv __pycache__ *.pyc
+python -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
 pip install --upgrade pip
@@ -42,8 +42,8 @@ python main.py
 cd -
 
 cd ./extractions/document/pdf/pymupdf4llm/
-rm -fr .venv
-python3.13 -m venv .venv
+rm -fr .venv __pycache__ *.pyc
+python -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
 pip install --upgrade pip
@@ -52,8 +52,8 @@ python main.py
 cd -
 
 cd ./extractions/document/pdf/unstructured/
-rm -fr .venv
-python3.13 -m venv .venv
+rm -fr .venv __pycache__ *.pyc
+python -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
 pip install --upgrade pip
@@ -61,6 +61,35 @@ pip install -r requirements.txt
 python main.py
 cd -
 
+cd ./extractions/document/pdf/pdfplumber/
+rm -fr .venv __pycache__ *.pyc
+python -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+pip install --upgrade pip
+pip install -r requirements.txt
+python main.py
+cd -
+
+cd ./extractions/document/pdf/pdfminer.six/
+rm -fr .venv __pycache__ *.pyc
+python -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+pip install --upgrade pip
+pip install -r requirements.txt
+python main.py
+cd -
+
+cd ./extractions/document/pdf/marker/
+rm -fr .venv __pycache__ *.pyc 
+python -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+pip install --upgrade pip
+pip install -r requirements.txt
+python main.py
+cd -
 
 
 
