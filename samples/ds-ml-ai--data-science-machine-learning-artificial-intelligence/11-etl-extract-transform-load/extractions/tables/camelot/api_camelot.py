d4f1b2f75b3537be4e23d9d4d6eb4b5d26a642f2
moljac <mcvjetko@holisticware.net>
2025-10-10 14:53:15 +0200

    camelot fixes

--- samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/tables/camelot/api_camelot.py
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/tables/camelot/api_camelot.py
@@ -13,10 +13,10 @@ def extract_tables_to_files (source: str) -> str:
     for i, table in enumerate(tables):
         # Extract table data as a Pandas DataFrame, including headers
         df = table.df                   # get a pandas DataFrame!
-        df.to_csv(f"{source}.page-.{i}.tables.camelot.csv")      # to_json, to_excel, to_html
-        df.to_json(f"{source}.page-.{i}.tables.camelot.json")    # to_json, to_excel, to_html
-        df.to_excel(f"{source}.page-.{i}.tables.camelot.xlsx")   # to_json, to_excel, to_html
-        df.to_html(f"{source}.page-.{i}.tables.camelot.html")    # to_json, to_excel, to_html
+        df.to_csv(f"{source}.tables-{i}.camelot.csv")      # to_json, to_excel, to_html
+        df.to_json(f"{source}.tables-{i}.camelot.json")    # to_json, to_excel, to_html
+        df.to_excel(f"{source}.tables-{i}.camelot.xlsx")   # to_json, to_excel, to_html
+        df.to_html(f"{source}.tables-{i}.camelot.html")    # to_json, to_excel, to_html
 
         pr = table.parsing_report
         with open(f"{source}.parsing_report.json", "w") as f:
