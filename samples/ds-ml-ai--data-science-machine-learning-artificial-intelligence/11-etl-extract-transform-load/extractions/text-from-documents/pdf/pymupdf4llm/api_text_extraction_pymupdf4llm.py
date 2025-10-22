8afeff9e3ddb575ffed8198467ca3b4876ae36ea
moljac <mcvjetko@holisticware.net>
2025-10-22 09:55:21 +0200

    pymupdf4llm

---
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/text-from-documents/pdf/pymupdf4llm/api_text_extraction_pymupdf4llm.py
@@ -0,0 +1,28 @@
+import pymupdf4llm
+
+
+def extract_markdown_to_file_from_pdf_document (source: str) -> str:
+
+    result_md = pymupdf4llm.to_markdown(source)
+
+    directory = f"{source}.hwaifs/text/python/"
+    Path(directory).mkdir(parents=True, exist_ok=True)
+
+    # save to file
+    with open(f"{directory}pymupdf4llm.md", "w") as f:
+        f.write(result_txt) 
+
+    return result_md
+
+def extract_markdown_to_file_from_office_doc_docx_document (source: str) -> str:
+
+    result_md = pymupdf4llm.to_markdown(source)
+
+    directory = f"{source}.hwaifs/text/python/"
+    Path(directory).mkdir(parents=True, exist_ok=True)
+
+    # save to file
+    with open(f"{directory}pymupdf4llm.md", "w") as f:
+        f.write(result_txt) 
+
+    return result_md
