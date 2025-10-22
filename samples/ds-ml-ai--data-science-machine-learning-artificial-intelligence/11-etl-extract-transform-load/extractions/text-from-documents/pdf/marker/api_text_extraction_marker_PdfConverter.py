daf5da03fc8b2ca38b8b4b5a7fb99921de75f537
moljac <mcvjetko@holisticware.net>
2025-10-22 09:51:56 +0200

    marker

---
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/text-from-documents/pdf/marker/api_text_extraction_marker_PdfConverter.py
@@ -0,0 +1,27 @@
+import os
+from pathlib import Path
+from marker.converters.pdf import PdfConverter
+from marker.models import create_model_dict
+from marker.output import text_from_rendered
+
+def extract_text_to_file_from_pdf_document (source: str) -> str:
+    converter = PdfConverter(
+        artifact_dict=create_model_dict(),
+    )
+    rendered = converter(source)
+    result_md, _, images = text_from_rendered(rendered)
+
+    directory = f"{source}.hwaifs/text/python/"
+    Path(directory).mkdir(parents=True, exist_ok=True)
+
+    # save to file
+    with open(f"{directory}/marker.md", "w") as f:
+        f.write(result_md)
+
+    directory = f"{source}.hwaifs/images/python/marker/"
+    Path(directory).mkdir(parents=True, exist_ok=True)
+
+    # print(images)
+
+    return result_md
+
