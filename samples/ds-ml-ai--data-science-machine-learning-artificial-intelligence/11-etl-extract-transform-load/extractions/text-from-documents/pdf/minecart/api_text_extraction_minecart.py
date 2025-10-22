d4bea47b1b6245c7ec748e1d29c466cf04de513d
moljac <mcvjetko@holisticware.net>
2025-10-22 09:54:05 +0200

    minecart

---
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/text-from-documents/pdf/minecart/api_text_extraction_minecart.py
@@ -0,0 +1,22 @@
+import os
+from pathlib import Path
+
+def extract_text_to_file_from_pdf_document (source: str) -> str:
+    pdf = open(f"{source}", 'rb')
+    doc = minecart.Document(pdf)
+
+    for page in doc.iter_pages():
+        page.
+    # for shape in page.shapes.iter_in_bbox((0, 0, 100, 200)):
+
+    # im = page.images[0].as_pil()  # requires pillow
+    # im.show()
+
+    directory = f"{source}.hwaifs/text/python/"
+    Path(directory).mkdir(parents=True, exist_ok=True)
+
+    # save to file
+    with open(f"{directory}/markitdown.md", "w") as f:
+        f.write(result_md)
+
+    return result_md
