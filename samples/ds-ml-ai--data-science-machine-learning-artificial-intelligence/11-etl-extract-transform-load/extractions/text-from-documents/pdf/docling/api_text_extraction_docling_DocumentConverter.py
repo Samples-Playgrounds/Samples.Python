f9e4581e85373918ff384f73516715bcff14a6c2
moljac <mcvjetko@holisticware.net>
2025-10-22 09:51:45 +0200

    docling

---
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/text-from-documents/pdf/docling/api_text_extraction_docling_DocumentConverter.py
@@ -0,0 +1,109 @@
+from docling.document_converter import DocumentConverter
+
+
+def extract_text_to_file_from_any_document (source: str) -> str:
+
+    converter = DocumentConverter()
+    result_doc = converter.convert(source)
+    result_txt = result_doc.document.export_to_text()
+
+    directory = f"{source}.hwaifs/text/python/"
+    Path(directory).mkdir(parents=True, exist_ok=True)
+
+    # save to file
+    with open(f"{directory}/docling.txt", "w") as f:
+        f.write(result_txt) 
+
+    return result_txt
+
+
+def extract_markdown_to_file_from_any_document (source: str) -> str:
+
+    converter = DocumentConverter()
+    result_doc = converter.convert(source)
+    result_md = result_doc.document.export_to_markdown()
+
+    directory = f"{source}.hwaifs/text/python/"
+    Path(directory).mkdir(parents=True, exist_ok=True)
+
+    # save to file
+    with open(f"{directory}/docling.md", "w") as f:
+        f.write(result_md)
+
+    return result_md
+
+from pathlib import Path
+from docling.datamodel.document import InputDocument
+from docling.datamodel.base_models import InputFormat
+#from docling.document_backends.ms_word import MsWordDocumentBackend
+
+from docling.backend.pypdfium2_backend import PyPdfiumDocumentBackend
+from docling.backend.msword_backend import MsWordDocumentBackend
+from docling.backend.msexcel_backend import MsExcelDocumentBackend
+from docling.document_converter import (
+    DocumentConverter,
+    PdfFormatOption,
+    WordFormatOption,
+    ExcelFormatOption
+)
+from docling.pipeline.simple_pipeline import SimplePipeline
+from docling.pipeline.standard_pdf_pipeline import StandardPdfPipeline
+from docling.datamodel.pipeline_options import PipelineOptions, EasyOcrOptions, TesseractOcrOptions
+from docling.datamodel.base_models import ConversionStatus, InputFormat
+
+def extract_markdown_to_file_from_any_document_complex (source: str) -> str:
+    doc_converter = (
+                DocumentConverter(  # all of the below is optional, has internal defaults.
+                allowed_formats=[
+                    InputFormat.PDF,
+                    InputFormat.IMAGE,
+                    InputFormat.DOCX,
+                    InputFormat.HTML,
+                    InputFormat.PPTX,
+                    InputFormat.ASCIIDOC,
+                    InputFormat.CSV,
+                    InputFormat.MD,
+                    InputFormat.XLSX
+                ],  # whitelist formats, non-matching files are ignored.
+                format_options={
+                    InputFormat.PDF: PdfFormatOption(
+                        pipeline_cls=StandardPdfPipeline, backend=PyPdfiumDocumentBackend
+                    ),
+                    InputFormat.DOCX: WordFormatOption(
+                        pipeline_cls=SimplePipeline  , backend=MsWordDocumentBackend
+                    ),
+                    InputFormat.XLSX: ExcelFormatOption(
+                        pipeline_cls=SimplePipeline  , backend=MsExcelDocumentBackend
+                    ),
+                },
+            )
+        )
+
+    conv_results = doc_converter.convert_all([source])
+
+    result_md = ""
+
+    for res in conv_results:
+        if res.document is None:
+            print(f"Document {res.input.file.name} could not be converted.")
+            continue
+        result_md += res.document.export_to_markdown()
+        
+        # out_path = Path("allouts")
+        # print(
+        #     f"Document {res.input.file.name} converted."
+        #     f"\nSaved markdown output to: {str(out_path)}"
+        # )
+        # _log.debug(res.document._export_to_indented_text(max_text_len=16))
+        # # Export Docling document format to markdowndoc:
+        #with (out_path / f"{res.input.file.stem}.md").open("w") as fp:
+        #    fp.write(res.document.export_to_markdown())
+
+    directory = f"{source}.hwaifs/text/python/"
+    Path(directory).mkdir(parents=True, exist_ok=True)
+
+    # save to file
+    with open(f"{directory}docling.filetype-agnostic.md", "w") as f:
+        f.write(result_md)
+
+    return result_md
---
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/text-from-documents/pdf/docling/readme.md
@@ -0,0 +1,54 @@
+# Docling
+
+readme.md
+
+```bash
+python -m venv .venv
+source .venv/bin/activate
+
+pip install docling
+
+pip freeze > requirements.txt
+```
+
+```bash
+pip install -r requirements.txt
+python convert.py
+```
+
+*   https://docling-project.github.io/docling/
+
+*   https://alain-airom.medium.com/my-first-hands-on-experience-with-docling-ed96eb7f864b
+
+*   https://www.reddit.com/r/LocalLLaMA/comments/1ghbmoq/docling_is_a_new_library_from_ibm_that/
+
+*   https://arxiv.org/abs/2501.17887
+
+*   https://alain-airom.medium.com/batch-document-conversion-using-docling-46abf8764b04
+
+*   https://research.ibm.com/publications/docling-an-efficient-open-source-toolkit-for-ai-driven-document-conversion
+
+*   Docling: Efficient document processing for AI workflows
+
+    *   https://www.youtube.com/watch?v=zSCxbqgqeJ8
+
+*   https://huggingface.co/ds4sd
+
+*   https://pypi.org/project/docling/
+
+*   https://explosion.ai/blog/pdfs-nlp-structured-data
+
+*   https://github.com/nunombispo/IBMDocling-LLMWhisperer
+
+*   https://news.ycombinator.com/item?id=42410803
+
+*   https://dev.to/aairom/using-doclings-ocr-features-with-rapidocr-29hd
+
+*   https://blog.gopenai.com/using-doclings-ocr-features-with-rapidocr-a757fbc1e7c8
+
+*   https://alain-airom.medium.com/my-first-hands-on-experience-with-docling-ed96eb7f864b
+
+*   https://python.langchain.com/docs/integrations/document_loaders/microsoft_word/
+
+*   https://github.com/mkumar-improva/docAi/blob/3dfdc173bf5c173f835240267d866278ab60245a/docling_sample.py#L6
+    
