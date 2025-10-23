ea24e87784d74faff478300a4ebeaedb6732dc06
moljac <mcvjetko@holisticware.net>
2025-10-23 08:07:07 +0200

    pypdf2

---
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/text-from-documents/pdf/PyPDF2/api_text_extraction_PyPDF2.py
@@ -0,0 +1,27 @@
+try:
+    import Image
+except ImportError:
+    from PIL import Image
+
+import pytesseract
+import PyPDF2
+import os
+from pathlib import Path
+
+
+from PyPDF2 import PdfReader
+
+def extract_text_to_file_from_pdf_document (source: str) -> str:
+    reader = PdfReader(source)
+    result_txt = ""
+    for page in reader.pages:
+        result_txt = result_txt + page.extract_text()
+
+    directory = f"{source}.hwaifs/text/python/"
+    Path(directory).mkdir(parents=True, exist_ok=True)
+
+    # save to file
+    with open(f"{directory}PyPDF2.txt", "w") as f:
+        f.write(result_txt) 
+
+    return result_txt
