7feffb3c79dc9ef5e32549d9256daf89b3f90c8a
moljac <mcvjetko@holisticware.net>
2025-10-29 10:28:17 +0100

    Update test.sh

--- samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/test.sh
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/test.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-figlet text-from-documents
-cd ./text-from-documents
+figlet text-from-docs
+cd ./text-from-documents/
 source ./test.sh
 cd -
+
+figlet images-from-docs
+cd ./images-from-documents/
+source ./test.sh
+cd -
+
+figlet tables-from-docs
+cd ./tables-from-documents/
+source ./test.sh
+cd -
+
+cd ..
