0669462f3d90bc30677f39dd6f1da5c8188523e6
moljac <mcvjetko@holisticware.net>
2025-11-04 20:17:32 +0100

    summarization ollama refactoring

---
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/summarizations-synopsizations/ollama-chat/main.py
@@ -0,0 +1,14 @@
+import ollama
+
+response = ollama.chat(
+    model="llama3.2-vision:latest",
+    messages=[
+        {
+            "role": "user",
+            "content": "Describe the image",
+            "images": ["./cat.jpeg"]}
+    ],
+)
+
+print(response["message"]['role'])
+print(response["message"]['content'])
