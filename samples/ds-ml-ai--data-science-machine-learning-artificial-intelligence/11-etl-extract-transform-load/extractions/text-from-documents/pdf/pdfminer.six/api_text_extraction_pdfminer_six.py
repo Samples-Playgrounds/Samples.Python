4c49ff82e8385c06f25f0e2edb028d1dde670e6e
moljac <mcvjetko@holisticware.net>
2025-11-06 12:55:05 +0100

    Update api_text_extraction_pdfminer_six.py

--- samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/text-from-documents/pdf/pdfminer.six/api_text_extraction_pdfminer_six.py
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/text-from-documents/pdf/pdfminer.six/api_text_extraction_pdfminer_six.py
@@ -6,11 +6,11 @@ def extract_text_to_file_from_pdf_document (source: str) -> str:
 
     result_txt = extract_text(source)
 
-    directory = f"{source}.hwaifs/text/python/"
+    directory = f"{source}.hwaifs/text/python/pdfminer_six/"
     Path(directory).mkdir(parents=True, exist_ok=True)
 
     # save to file
-    with open(f"{directory}/pdfminer_six.txt", "w") as f:
+    with open(f"{directory}/content.txt", "w") as f:
         f.write(result_txt)
 
     return result_txt
