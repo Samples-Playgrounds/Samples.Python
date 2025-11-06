d0e96bdbb798ab5878c8d3bfff53b95ce42c19d3
moljac <mcvjetko@holisticware.net>
2025-11-06 12:54:51 +0100

    Update api_text_extraction_PyPDF2.py

--- samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/text-from-documents/pdf/PyPDF2/api_text_extraction_PyPDF2.py
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/text-from-documents/pdf/PyPDF2/api_text_extraction_PyPDF2.py
@@ -17,11 +17,11 @@ def extract_text_to_file_from_pdf_document (source: str) -> str:
     for page in reader.pages:
         result_txt = result_txt + page.extract_text()
 
-    directory = f"{source}.hwaifs/text/python/"
+    directory = f"{source}.hwaifs/text/python/PyPDF2/"
     Path(directory).mkdir(parents=True, exist_ok=True)
 
     # save to file
-    with open(f"{directory}PyPDF2.txt", "w") as f:
+    with open(f"{directory}/content.txt", "w") as f:
         f.write(result_txt) 
 
     return result_txt
