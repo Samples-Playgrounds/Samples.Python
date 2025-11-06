ab61ddd130c3dbfd9b7bb926a5f4da9041c01622
moljac <mcvjetko@holisticware.net>
2025-11-06 12:54:46 +0100

    Update api_text_extraction_pytesseract.py

--- samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/text-from-documents/pdf/pytesseract/api_text_extraction_pytesseract.py
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/text-from-documents/pdf/pytesseract/api_text_extraction_pytesseract.py
@@ -11,11 +11,11 @@ def extract_text_to_file_from_pdf_document (source: str) -> str:
     for pageNum,imgBlob in enumerate(pages):
         result_txt = result_txt + pytesseract.image_to_string(imgBlob,lang='eng')
 
-    directory = f"{source}.hwaifs/text/python/"
+    directory = f"{source}.hwaifs/text/python/pytesseract/"
     Path(directory).mkdir(parents=True, exist_ok=True)
 
     # save to file
-    with open(f"{directory}pytesseract.txt", "w") as f:
+    with open(f"{directory}/content.txt", "w") as f:
         f.write(result_txt) 
 
     return result_txt
