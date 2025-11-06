f7a66737d8bbe57bd663ea63d9a70bdd4b40d3f8
moljac <mcvjetko@holisticware.net>
2025-11-06 12:55:15 +0100

    Update api_text_extraction_docling_DocumentConverter.py

--- samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/text-from-documents/pdf/docling/api_text_extraction_docling_DocumentConverter.py
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/text-from-documents/pdf/docling/api_text_extraction_docling_DocumentConverter.py
@@ -7,11 +7,11 @@ def extract_text_to_file_from_any_document (source: str) -> str:
     result_doc = converter.convert(source)
     result_txt = result_doc.document.export_to_text()
 
-    directory = f"{source}.hwaifs/text/python/"
+    directory = f"{source}.hwaifs/text/python/docling/"
     Path(directory).mkdir(parents=True, exist_ok=True)
 
     # save to file
-    with open(f"{directory}/docling.txt", "w") as f:
+    with open(f"{directory}/content.txt", "w") as f:
         f.write(result_txt) 
 
     return result_txt
@@ -23,11 +23,11 @@ def extract_markdown_to_file_from_any_document (source: str) -> str:
     result_doc = converter.convert(source)
     result_md = result_doc.document.export_to_markdown()
 
-    directory = f"{source}.hwaifs/text/python/"
+    directory = f"{source}.hwaifs/text/python/docling/"
     Path(directory).mkdir(parents=True, exist_ok=True)
 
     # save to file
-    with open(f"{directory}/docling.md", "w") as f:
+    with open(f"{directory}/content.md", "w") as f:
         f.write(result_md)
 
     return result_md
@@ -99,11 +99,11 @@ def extract_markdown_to_file_from_any_document_complex (source: str) -> str:
         #with (out_path / f"{res.input.file.stem}.md").open("w") as fp:
         #    fp.write(res.document.export_to_markdown())
 
-    directory = f"{source}.hwaifs/text/python/"
+    directory = f"{source}.hwaifs/text/python/docling/"
     Path(directory).mkdir(parents=True, exist_ok=True)
 
     # save to file
-    with open(f"{directory}docling.filetype-agnostic.md", "w") as f:
+    with open(f"{directory}content.filetype-agnostic.md", "w") as f:
         f.write(result_md)
 
     return result_md
