fb221cbe32071914b978f8bdb97bdc5fcd3c32c0
moljac <mcvjetko@holisticware.net>
2025-11-06 12:55:09 +0100

    Update api_text_extraction_markitdown.py

--- samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/text-from-documents/pdf/markitdown/api_text_extraction_markitdown.py
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/text-from-documents/pdf/markitdown/api_text_extraction_markitdown.py
@@ -6,11 +6,11 @@ def extract_text_to_file_from_any_document (source: str) -> str:
     md = MarkItDown()
     result_md = md.convert(source).text_content
 
-    directory = f"{source}.hwaifs/text/python/"
+    directory = f"{source}.hwaifs/text/python/markitdown/"
     Path(directory).mkdir(parents=True, exist_ok=True)
 
     # save to file
-    with open(f"{directory}/markitdown.md", "w") as f:
+    with open(f"{directory}/content.md", "w") as f:
         f.write(result_md)
 
     return result_md
