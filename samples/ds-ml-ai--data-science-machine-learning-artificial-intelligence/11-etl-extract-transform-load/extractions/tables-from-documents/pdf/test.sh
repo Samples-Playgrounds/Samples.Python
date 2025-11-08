672b576152445c468849fb8ff4a481727ffe7c2e
moljac <mcvjetko@holisticware.net>
2025-11-08 15:07:05 +0100

    Update test.sh

--- samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/tables-from-documents/pdf/test.sh
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/tables-from-documents/pdf/test.sh
@@ -2,6 +2,7 @@
 
 figlet gmft
 cd ./gmft/
+deactivate
 rm -fr .venv __pycache__ 
 rm *.pyc
 python -m venv .venv
@@ -12,16 +13,15 @@ pip install gmft
 pip install openpyxl
 pip install --upgrade pip
 python main.py
-cd -
+cd ..
 
 figlet tabula-py
 cd ./tabula-py/
+deactivate
 rm -fr .venv __pycache__ 
 rm *.pyc
 python -m venv .venv
 source .venv/bin/activate
-pip install tabula-py
-pip install tabula-py[jpype]
 pip install tabula-py
 pip install tabula-py[jpype]
 pip install jpype1
@@ -30,10 +30,11 @@ pip install tabulate
 pip install openpyxl
 pip install --upgrade pip
 python main.py
-cd -
+cd ..
 
 figlet docling
 cd ./docling/
+deactivate
 rm -fr .venv __pycache__ 
 rm *.pyc
 python -m venv .venv
@@ -42,10 +43,11 @@ pip install opencv-python
 pip install docling pandas
 pip install --upgrade pip
 python main.py
-cd -
+cd ..
 
 figlet camelot
 cd ./camelot/
+deactivate
 rm -fr .venv __pycache__ 
 rm *.pyc
 python -m venv .venv
@@ -53,10 +55,11 @@ source .venv/bin/activate
 pip install "camelot-py[base]"
 pip install -r requirements.txt
 python main.py
-cd -
+cd ..
 
 figlet pdfplumber
 cd ./pdfplumber/
+deactivate
 rm -fr .venv __pycache__ 
 rm *.pyc
 python -m venv .venv
@@ -68,23 +71,12 @@ pip install openpyxl
 pip install --upgrade pip
 pip install -r requirements.txt
 python main.py
-cd -
-
-figlet pdftables
-cd ./pdftables/
-rm -fr .venv __pycache__ 
-rm *.pyc
-python -m venv .venv
-source .venv/bin/activate
-pip install pdftables
-pip install --upgrade pip
-pip install -r requirements.txt
-python main.py
-cd -
+cd ..
 
 
 figlet marker
 cd ./marker/
+deactivate
 rm -fr .venv __pycache__ 
 rm *.pyc
 python -m venv .venv
@@ -94,4 +86,17 @@ pip install 'marker-pdf[full]'
 pip install --upgrade pip
 pip install -r requirements.txt
 python main.py
-cd -
+cd ..
+
+# figlet pdftables
+# cd ./pdftables/
+# rm -fr .venv __pycache__ 
+# rm *.pyc
+# python -m venv .venv
+# source .venv/bin/activate
+# pip install pdftables
+# pip install --upgrade pip
+# pip install -r requirements.txt
+# python main.py
+# cd ..
+
