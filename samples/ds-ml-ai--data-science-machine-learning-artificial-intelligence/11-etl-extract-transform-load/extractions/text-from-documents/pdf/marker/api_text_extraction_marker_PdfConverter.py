ee34856b592af13b3a44a5f18306bfe27f2c1f7a
moljac <mcvjetko@holisticware.net>
2025-11-13 19:24:12 +0100

    cleanup + fixes + refactoring + improvements for  marker

--- samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/text-from-documents/pdf/marker/api_text_extraction_marker_PdfConverter.py
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/text-from-documents/pdf/marker/api_text_extraction_marker_PdfConverter.py
@@ -4,7 +4,22 @@ from marker.converters.pdf import PdfConverter
 from marker.models import create_model_dict
 from marker.output import text_from_rendered
 
+import json
+import datetime
+import time
+from time import perf_counter
+from time import perf_counter_ns
+# from timer import timer
+
+#@timer()
 def extract_text_to_file_from_pdf_document (source: str) -> str:
+
+    #---------------------------------------------------------------------------
+    time_start_1 = time.time()
+    time_start_2 = perf_counter()
+    time_start_3 = perf_counter_ns()
+    #---------------------------------------------------------------------------
+
     converter = PdfConverter(
         artifact_dict=create_model_dict(),
     )
@@ -17,6 +32,32 @@ def extract_text_to_file_from_pdf_document (source: str) -> str:
     # save to file
     with open(f"{directory}/content.md", "w") as f:
         f.write(result_md)
+
+    #---------------------------------------------------------------------------
+    time_stop_1 = time.time()
+    time_total_1 = time_stop_1 - time_start_1
+    time_stop_2 = perf_counter()
+    time_total_2 = time_stop_2 - time_start_2
+    time_stop_3 = perf_counter_ns()
+    time_total_3 = (time_stop_3 - time_start_3) / 1_000_000_000
+
+    times = {
+        "function_method_name" : "extract_text_to_file_from_pdf_document",
+        "time_start_1": time_start_1,
+        "time_end_1": time_stop_1,
+        "time_total_1": time_total_1,
+        "time_start_2": time_start_2,
+        "time_end_2": time_stop_2,
+        "time_total_2": time_total_2,
+        "time_start_3": time_start_3,
+        "time_end_3": time_stop_3,
+        "time_total_3": time_total_3,
+    }
+
+    timestamp = datetime.datetime.now().isoformat().replace(":", "-")
+    with open(f"{directory}/performance-data-{timestamp}.python.json", "w") as f:
+        f.write(json.dumps(times, indent=4))
+    #---------------------------------------------------------------------------
 
     return result_md
 
---
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/text-from-documents/pdf/marker/test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+deactivate
+rm -fr .venv/ __pycache__/
+rm *.pyc
+
+
+
+python -m venv .venv
+source .venv/bin/activate
+
+pip install 'marker-pdf[full]'
+
+pip install timer
+pip install codetiming
+
+pip freeze > requirements.txt
+
+
+python main.py
