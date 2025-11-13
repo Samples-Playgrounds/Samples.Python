3e731a3a76754276a362c783b5040b2cf15fda7d
moljac <mcvjetko@holisticware.net>
2025-11-13 19:27:01 +0100

    cleanup + fixes + refactoring + improvements for  pytesseract

---
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/text-from-documents/pdf/pytesseract/test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+deactivate
+rm -fr .venv/ __pycache__/
+rm *.pyc
+
+
+
+python -m venv .venv
+source .venv/bin/activate
+
+pip install pytesseract
+pip install pdf2image
+
+pip install timer
+pip install codetiming
+
+pip freeze > requirements.txt
+
+
+python main.py
