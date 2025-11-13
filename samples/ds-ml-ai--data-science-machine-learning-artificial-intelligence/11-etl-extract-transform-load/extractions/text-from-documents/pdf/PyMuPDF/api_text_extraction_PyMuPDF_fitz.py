2bd9f7d20e311719a8a342bdbc13d735a3cbf50d
moljac <mcvjetko@holisticware.net>
2025-11-13 19:25:29 +0100

    cleanup + fixes + refactoring + improvements for PYmuPDF

--- samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/text-from-documents/pdf/PyMuPDF/api_text_extraction_PyMuPDF_fitz.py
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/text-from-documents/pdf/PyMuPDF/api_text_extraction_PyMuPDF_fitz.py
@@ -1,6 +1,14 @@
 import fitz  # PyMuPDF
+
 import os
 from pathlib import Path
+
+import json
+import datetime
+import time
+from time import perf_counter
+from time import perf_counter_ns
+# from timer import timer
 
 # https://pymupdf.readthedocs.io/en/latest/how-to-open-a-file.html#supported-file-types
 # Document Formats
@@ -41,8 +49,15 @@ from pathlib import Path
 #     PSD,
 #     PS
 
-
+#@timer()
 def extract_text_to_file_from_pdf_document (source: str) -> str:
+
+    #---------------------------------------------------------------------------
+    time_start_1 = time.time()
+    time_start_2 = perf_counter()
+    time_start_3 = perf_counter_ns()
+    #---------------------------------------------------------------------------
+
     doc = fitz.open(source)
     result_txt = ""
     for page in doc:
@@ -54,6 +69,32 @@ def extract_text_to_file_from_pdf_document (source: str) -> str:
     # save to file
     with open(f"{directory}/content.txt", "w") as f:
         f.write(result_txt) 
+
+    #---------------------------------------------------------------------------
+    time_stop_1 = time.time()
+    time_total_1 = time_stop_1 - time_start_1
+    time_stop_2 = perf_counter()
+    time_total_2 = time_stop_2 - time_start_2
+    time_stop_3 = perf_counter_ns()
+    time_total_3 = (time_stop_3 - time_start_3) / 1_000_000_000
+
+    times = {
+        "function_method_name" : "extract_text_to_file_from_any_document",
+        "time_start_1": time_start_1,
+        "time_end_1": time_stop_1,
+        "time_total_1": time_total_1,
+        "time_start_2": time_start_2,
+        "time_end_2": time_stop_2,
+        "time_total_2": time_total_2,
+        "time_start_3": time_start_3,
+        "time_end_3": time_stop_3,
+        "time_total_3": time_total_3,
+    }
+
+    timestamp = datetime.datetime.now().isoformat().replace(":", "-")
+    with open(f"{directory}/performance-data-{timestamp}.python.json", "w") as f:
+        f.write(json.dumps(times, indent=4))
+    #---------------------------------------------------------------------------
 
     return result_txt
 
