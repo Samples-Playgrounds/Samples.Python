e062beb3ebaf55c0006c03d59365e120fdf868a2
moljac <mcvjetko@holisticware.net>
2025-11-13 19:27:21 +0100

    cleanup + fixes + refactoring + improvements for unstructured

--- samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/text-from-documents/pdf/unstructured/api_text_extraction_unstructured_partition_pdf.py
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/text-from-documents/pdf/unstructured/api_text_extraction_unstructured_partition_pdf.py
@@ -1,10 +1,24 @@
 from unstructured.partition.pdf import partition_pdf
 from unstructured.staging.base import elements_to_json
+
 import os
 from pathlib import Path
 
+import json
+import datetime
+import time
+from time import perf_counter
+from time import perf_counter_ns
+# from timer import timer
 
+#@timer()
 def extract_text_to_file_from_pdf_document (source: str) -> str:
+
+    #---------------------------------------------------------------------------
+    time_start_1 = time.time()
+    time_start_2 = perf_counter()
+    time_start_3 = perf_counter_ns()
+    #---------------------------------------------------------------------------
 
     # Returns a List[Element] present in the pages of the parsed pdf document
     elements = partition_pdf(source)
@@ -22,5 +36,31 @@ def extract_text_to_file_from_pdf_document (source: str) -> str:
                         elements=elements, 
                         filename=f"{directory}contents.json",
                     )
+
+    #---------------------------------------------------------------------------
+    time_stop_1 = time.time()
+    time_total_1 = time_stop_1 - time_start_1
+    time_stop_2 = perf_counter()
+    time_total_2 = time_stop_2 - time_start_2
+    time_stop_3 = perf_counter_ns()
+    time_total_3 = (time_stop_3 - time_start_3) / 1_000_000_000
+
+    times = {
+        "function_method_name" : "extract_text_to_file_from_any_document",
+        "time_start_1": time_start_1,
+        "time_end_1": time_stop_1,
+        "time_total_1": time_total_1,
+        "time_start_2": time_start_2,
+        "time_end_2": time_stop_2,
+        "time_total_2": time_total_2,
+        "time_start_3": time_start_3,
+        "time_end_3": time_stop_3,
+        "time_total_3": time_total_3,
+    }
+
+    timestamp = datetime.datetime.now().isoformat().replace(":", "-")
+    with open(f"{directory}/performance-data-{timestamp}.python.json", "w") as f:
+        f.write(json.dumps(times, indent=4))
+    #---------------------------------------------------------------------------
 
     return result_txt
