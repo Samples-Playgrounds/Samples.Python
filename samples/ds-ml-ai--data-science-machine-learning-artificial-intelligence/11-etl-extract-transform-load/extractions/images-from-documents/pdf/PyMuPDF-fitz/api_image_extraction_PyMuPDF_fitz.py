b7d54ab2e2d2863dfa2392fe244aa51da8fb0d2d
moljac <mcvjetko@holisticware.net>
2025-11-14 12:31:19 +0100

    images from docs fixes, refactoring PymuPDF fitz

---
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/images-from-documents/pdf/PyMuPDF-fitz/api_image_extraction_PyMuPDF_fitz.py
@@ -0,0 +1,61 @@
+import fitz as mu  # PyMuPDF
+
+from pathlib import Path
+
+import json
+import datetime
+import time
+from time import perf_counter
+from time import perf_counter_ns
+# from timer import timer
+
+#@timer()
+def extract_images_from_pdf_to_files (source: str) -> str:
+    """
+    """
+
+    #---------------------------------------------------------------------------
+    time_start_1 = time.time()
+    time_start_2 = perf_counter()
+    time_start_3 = perf_counter_ns()
+    #---------------------------------------------------------------------------
+
+    directory = f"{source}.hwaifs/images/python/PyMuPDF-fitz/"
+    Path(directory).mkdir(parents=True, exist_ok=True)
+
+    with mu.open(source) as pdf:
+        for page in pdf:
+            for info in page.get_images(full=True):
+                xref = info[0]
+                img = pdf.extract_image(xref)
+
+                ext, data = img['ext'], img['image']
+
+                with open(f'{directory}/{xref}.{ext}', 'wb') as f:
+                    f.write(data)
+
+    #---------------------------------------------------------------------------
+    time_stop_1 = time.time()
+    time_total_1 = time_stop_1 - time_start_1
+    time_stop_2 = perf_counter()
+    time_total_2 = time_stop_2 - time_start_2
+    time_stop_3 = perf_counter_ns()
+    time_total_3 = (time_stop_3 - time_start_3) / 1_000_000_000
+
+    times = {
+        "function_method_name" : "extract_text_to_file_from_any_document",
+        "time_start_1": time_start_1,
+        "time_end_1": time_stop_1,
+        "time_total_1": time_total_1,
+        "time_start_2": time_start_2,
+        "time_end_2": time_stop_2,
+        "time_total_2": time_total_2,
+        "time_start_3": time_start_3,
+        "time_end_3": time_stop_3,
+        "time_total_3": time_total_3,
+    }
+
+    timestamp = datetime.datetime.now().isoformat().replace(":", "-")
+    with open(f"{directory}/performance-data-{timestamp}.python.json", "w") as f:
+        f.write(json.dumps(times, indent=4))
+    #---------------------------------------------------------------------------
