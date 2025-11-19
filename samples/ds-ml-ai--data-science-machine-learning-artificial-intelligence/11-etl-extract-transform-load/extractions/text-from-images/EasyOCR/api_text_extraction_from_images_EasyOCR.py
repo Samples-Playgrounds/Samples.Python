824dc9a03533a5d7f2fd2fd0b4e82be547f8c119
moljac <mcvjetko@holisticware.net>
2025-11-19 08:46:05 +0100

    text from images EasyOCR

---
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/text-from-images/EasyOCR/api_text_extraction_from_images_EasyOCR.py
@@ -0,0 +1,71 @@
+import easyocr
+
+import os
+from pathlib import Path
+
+import json
+import datetime
+import time
+from time import perf_counter
+from time import perf_counter_ns
+# from timer import timer
+
+#@timer()
+def extract_text_to_file_from_image (source: str) -> str:
+
+    #---------------------------------------------------------------------------
+    time_start_1 = time.time()
+    time_start_2 = perf_counter()
+    time_start_3 = perf_counter_ns()
+    #---------------------------------------------------------------------------
+
+    directory = f"{source}.hwaifs/text/python/easyocr/"
+    Path(directory).mkdir(parents=True, exist_ok=True)
+
+    # https://www.jaided.ai/easyocr/modelhub/
+
+    reader = easyocr.Reader(
+                            [
+                                'en',
+                                'hr'
+                            ]
+        ) # this needs to run only once to load the model into memory
+    result = reader.readtext(source)
+
+    text_complete = ""
+
+    for (bbox, text, prob) in result:
+        text_complete += text + '\n'
+
+    # save to file
+    with open(f"{directory}/content.txt", "w") as f:
+        f.write(text_complete) 
+
+    #---------------------------------------------------------------------------
+    time_stop_1 = time.time()
+    time_total_1 = time_stop_1 - time_start_1
+    time_stop_2 = perf_counter()
+    time_total_2 = time_stop_2 - time_start_2
+    time_stop_3 = perf_counter_ns()
+    time_total_3 = (time_stop_3 - time_start_3) / 1_000_000_000
+
+    times = {
+        "function_method_name" : "extract_text_to_file_from_image",
+        "time_start_1": time_start_1,
+        "time_end_1": time_stop_1,
+        "time_total_1": time_total_1,
+        "time_start_2": time_start_2,
+        "time_end_2": time_stop_2,
+        "time_total_2": time_total_2,
+        "time_start_3": time_start_3,
+        "time_end_3": time_stop_3,
+        "time_total_3": time_total_3,
+    }
+
+    timestamp = datetime.datetime.now().isoformat().replace(":", "-")
+    with open(f"{directory}/performance-data-{timestamp}.python.json", "w") as f:
+        f.write(json.dumps(times, indent=4))
+    #---------------------------------------------------------------------------
+
+    return text_complete
+
