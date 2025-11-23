ccce14ff08abf792282e9ee6db7b345762b66ad8
moljac <mcvjetko@holisticware.net>
2025-11-23 23:37:22 +0100

    chunking splitting by characters refactoring

--- samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/21-splitting-chunking/character-splitting/api_cunking_splitting_by_characters.py
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/21-splitting-chunking/character-splitting/api_cunking_splitting_by_characters.py
@@ -3,4 +3,5 @@ def split_on_character (text: str, size_chunk: int = 35) -> []:
     result = []
     for i in range(0, len(text), size_chunk):
         result.append(text[i:i + size_chunk])
+        
     return result
