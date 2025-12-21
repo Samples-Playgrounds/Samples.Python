267669d59bb8f7cf9bf3b6011d8578515bcc1153
moljac <mcvjetko@holisticware.net>
2025-12-21 21:57:20 +0100

    Update api_cunking_splitting_by_characters.py

--- samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/21-splitting-chunking/character-splitting/api_cunking_splitting_by_characters.py
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/21-splitting-chunking/character-splitting/api_cunking_splitting_by_characters.py
@@ -1,7 +1,31 @@
 
-def split_on_character (text: str, size_chunk: int = 35) -> []:
+def split_chunks_on_character_count (
+                                        text: str,
+                                        size_chunk: int = 100 
+                                    ) -> [] :
+    """
+    """
+    
     result = []
     for i in range(0, len(text), size_chunk):
         result.append(text[i:i + size_chunk])
         
+    return result
+
+
+def split_chunks_on_character_count_with_overlap (
+                                                    text: str, 
+                                                    size_chunk: int = 100,
+                                                    size_overlap: int = 0
+                                                ) -> []:
+    """
+    """
+    
+    result = []
+    for i in range(0, len(text),size_chunk):
+        b = i
+        e = b + size_chunk - size_overlap
+        result.append(text[b:e])
+        b = 
+
     return result
