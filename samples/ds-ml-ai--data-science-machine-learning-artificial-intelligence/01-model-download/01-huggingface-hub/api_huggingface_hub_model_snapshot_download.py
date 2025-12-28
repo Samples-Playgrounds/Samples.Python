50828d687f66b51791a3c012995c5a758687490a
moljac <mcvjetko@holisticware.net>
2025-12-28 21:39:44 +0100

    Update api_huggingface_hub_model_snapshot_download.py

--- samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/01-model-download/01-huggingface-hub/api_huggingface_hub_model_snapshot_download.py
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/01-model-download/01-huggingface-hub/api_huggingface_hub_model_snapshot_download.py
@@ -1,13 +1,29 @@
 from huggingface_hub import snapshot_download
 from huggingface_hub import hf_hub_download
+
 import joblib
 
+import os
+from pathlib import Path
+
+import json
+import datetime
+import time
+from time import perf_counter
+from time import perf_counter_ns
+
 def download_model_to_folder(
-                    repo_id: str, 
-                    local_dir:"vicuna-hf",
-                    local_dir_use_symlinks=False,
-                    revision="main"
-                    ) -> []:
+                                repo_id: str, 
+                                local_dir:"models",
+                                local_dir_use_symlinks=False,
+                                revision="main"
+                            ) -> []:
+
+    #---------------------------------------------------------------------------
+    time_start_1 = time.time()
+    time_start_2 = perf_counter()
+    time_start_3 = perf_counter_ns()
+    #---------------------------------------------------------------------------
 
     snapshot_download(
                         repo_id=repo_id,
@@ -16,18 +32,87 @@ def download_model_to_folder(
                         revision=revision
                     )
 
+    directory = f".hwaifs/models/"
+    Path(directory).mkdir(parents=True, exist_ok=True)
 
-from huggingface_hub import hf_hub_download
+    #---------------------------------------------------------------------------
+    time_stop_1 = time.time()
+    time_total_1 = time_stop_1 - time_start_1
+    time_stop_2 = perf_counter()
+    time_total_2 = time_stop_2 - time_start_2
+    time_stop_3 = perf_counter_ns()
+    time_total_3 = (time_stop_3 - time_start_3) / 1_000_000_000
+
+    times = {
+        "function_method_name" : "extract_text_to_file_from_any_document",
+        "time_start_1": time_start_1,
+        "time_end_1": time_stop_1,
+        "time_total_1": time_total_1,
+        "time_start_2": time_start_2,
+        "time_end_2": time_stop_2,
+        "time_total_2": time_total_2,
+        "time_start_3": time_start_3,
+        "time_end_3": time_stop_3,
+        "time_total_3": time_total_3,
+    }
+
+    timestamp = datetime.datetime.now().isoformat().replace(":", "-")
+    with open(f"{directory}/performance-data-{timestamp}.python.json", "w") as f:
+        f.write(json.dumps(times, indent=4))
+    #---------------------------------------------------------------------------
+
+    return
+
 
 def download_hf_model_to_folder(
-                                repo_id: str, 
-                                file_name: str,
-                                use_auth_token: bool
+                                    repo_id: str, 
+                                    file_name: str,
+                                    use_auth_token: bool
                                 ) -> []:
 
+    #---------------------------------------------------------------------------
+    time_start_1 = time.time()
+    time_start_2 = perf_counter()
+    time_start_3 = perf_counter_ns()
+    #---------------------------------------------------------------------------
+
     downloaded_model_path = hf_hub_download(
-                                            repo_id=repo_id,
-                                            filename=file_name,
-                                            use_auth_token=use_auth_token
-                                            ); 
+                                                repo_id=repo_id,
+                                                filename=file_name,
+                                                use_auth_token=use_auth_token
+                                            );
+
+    directory = f"{source}.hwaifs/models/"
+    Path(directory).mkdir(parents=True, exist_ok=True)
+
+    # save to file
+    with open(f"{directory}/content.txt", "w") as f:
+        f.write(result_txt) 
+
+    #---------------------------------------------------------------------------
+    time_stop_1 = time.time()
+    time_total_1 = time_stop_1 - time_start_1
+    time_stop_2 = perf_counter()
+    time_total_2 = time_stop_2 - time_start_2
+    time_stop_3 = perf_counter_ns()
+    time_total_3 = (time_stop_3 - time_start_3) / 1_000_000_000
+
+    times = {
+        "function_method_name" : "extract_text_to_file_from_any_document",
+        "time_start_1": time_start_1,
+        "time_end_1": time_stop_1,
+        "time_total_1": time_total_1,
+        "time_start_2": time_start_2,
+        "time_end_2": time_stop_2,
+        "time_total_2": time_total_2,
+        "time_start_3": time_start_3,
+        "time_end_3": time_stop_3,
+        "time_total_3": time_total_3,
+    }
+
+    timestamp = datetime.datetime.now().isoformat().replace(":", "-")
+    with open(f"{directory}/performance-data-{timestamp}.python.json", "w") as f:
+        f.write(json.dumps(times, indent=4))
+    #---------------------------------------------------------------------------
+
     return downloaded_model_path
