3f22ab9a64081986eb87b5ae8cb0906614f936bd
moljac <mcvjetko@holisticware.net>
2025-12-28 21:39:48 +0100

    Update main.py

--- samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/01-model-download/01-huggingface-hub/main.py
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/01-model-download/01-huggingface-hub/main.py
@@ -1,16 +1,39 @@
 """
-rm -fr .venv
-python -m venv .venv
-source .venv/bin/activate
-pip install "huggingface_hub[hf_transfer]"  joblib
-pip freeze > requirements.txt
-
-pip install "huggingface_hub[hf_transfer]"
-HF_HUB_ENABLE_HF_TRANSFER=1 hf download ...
+deactivate
+rm -fr .venv/ __pycache__/
+rm *.pyc
 """
 
-import api_huggingface_hub_model_snapshot_download as api
+"""
+python -m venv .venv
+source .venv/bin/activate
+pip install "huggingface_hub[hf_transfer]"
+pip install joblib
+pip install timer
+pip install codetiming
+pip freeze > requirements.txt
+"""
 
+"""
+pip install -r requirements.txt
+python main.py
+"""
+
+"""
+HF_HUB_ENABLE_HF_TRANSFER=1 hf download
+"""
+
+# https://huggingface.co/models?search=csharp
+# https://huggingface.co/models?sort=created&search=python
+# https://huggingface.co/models?p=1&sort=created&search=android
+models = [
+    "Alexis-Az/Qwen-2.5-Coder-7B-4bit-CSharp-Alpaca-Instruct",
+    "Alexis-Az/Qwen-2.5-Coder-7B-4bit-CSharp-Alpaca-Code-ORPO-LoRA",
+    "Alexis-Az/Qwen-2.5-Coder-7B-4bit-CSharp",
+    "Safurai/Safurai-Csharp-34B",
+    "Safurai/Safurai-Csharp-34B-GGUF",
+]
+import api_huggingface_hub_model_snapshot_download as api
 
 #model_id="lmsys/vicuna-13b-v1.5"
 #api.download_model_to_folder(
@@ -20,11 +43,21 @@ import api_huggingface_hub_model_snapshot_download as api
 #                                revision="main"
 #                            )
 
-model_id="CompVis/stable-diffusion-v-1-4-original"
-file = api.download_hf_model_to_folder(
-                                        repo_id=model_id,
-                                        file_name="sd-v1-4.ckpt",
-                                        use_auth_token=False
-                                    )
+# model_id="CompVis/stable-diffusion-v-1-4-original"
+# file = api.download_hf_model_to_folder(
+#                                         repo_id=model_id,
+#                                         file_name="sd-v1-4.ckpt",
+#                                         use_auth_token=False
+#                                     )
 
-print(file)
+
+def main():
+   for model in models:
+      print(f"huggingface_hub <- downloadin = {model}")
+      result_txt = api.download_model_to_folder(
+                                                    model,
+                                                    local_dir=".hwaifs/models/"
+                                                )
+
+if __name__ == '__main__':
+    main()
