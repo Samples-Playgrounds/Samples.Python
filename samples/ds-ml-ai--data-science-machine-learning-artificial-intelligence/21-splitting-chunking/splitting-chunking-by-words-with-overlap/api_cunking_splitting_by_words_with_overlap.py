d67e7b338120db5199392ed92049872223536400
moljac <mcvjetko@holisticware.net>
2026-01-13 22:02:51 +0100

    splitting/chunking by words length and overlap

---
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/21-splitting-chunking/splitting-chunking-by-words-with-overlap/api_cunking_splitting_by_words_with_overlap.py
@@ -0,0 +1,62 @@
+import os
+from pathlib import Path
+
+import json
+import datetime
+import time
+from time import perf_counter
+from time import perf_counter_ns
+# from timer import timer
+
+def text_split_chunks_on_word_count_with_overlap (
+                                        text: str,
+                                        size_chunk: int = 500,
+                                        size_overlap: int = 50
+
+                                    ) -> [] :
+    """
+    Splits text into chunks of a specified word count with overlap.
+    """
+    
+    words = text.split()
+    result = []
+    for i in range(0, len(words), size_chunk - size_overlap):
+        result.append(' '.join(words[i:i + size_chunk]))
+
+    return result
+
+
+import json
+
+def file_split_chunks_on_word_count_with_overlap (
+                                                    file_name: str,
+                                                    size_chunk: int = 500,
+                                                    size_overlap: int = 50
+
+                                                ) -> [] :
+    """
+    Splits file into chunks of a specified word count with overlap.
+    """
+    
+    if (file_name is None) or (not os.path.isfile(file_name)):
+        # raise ValueError("file_name must be a valid file path.")
+        print(f"Error not found {file_name}")
+        return None
+
+    directory = f"{file_name}.hwaifs/{file_name}.hwaifs/splits-chunks/py/"
+    Path(directory).mkdir(parents=True, exist_ok=True)
+
+    with open(file_name, 'r', encoding='utf-8') as f:
+        text = f.read()
+
+    result = text_split_chunks_on_word_count_with_overlap(text, size_chunk, size_overlap)
+
+    file_name_result = f"{directory}/chunks_on_word_count_with_overlap-{size_chunk}-{size_overlap}.txt"
+    with open(file_name_result, 'w', encoding='utf-8') as f:
+        f.write(str(result))
+
+    file_name_result = f"{directory}/chunks_on_word_count_with_overlap-{size_chunk}-{size_overlap}.json"
+    with open(file_name_result, 'w', encoding='utf-8') as f:
+        json.dump(result, f)
+
+    return result
---
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/21-splitting-chunking/splitting-chunking-by-words-with-overlap/readme.md
@@ -0,0 +1,8 @@
+# Splitting/Chunking by words with overlap
+
+*   https://medium.com/@hemanthgk/using-ollama-to-build-a-local-search-engine-for-your-files-d8a88f55cd51
+
+*   https://github.com/imanoop7/Generative-Search-Engine-For-Local-Files
+
+*   https://pub.towardsai.net/building-your-own-generative-search-engine-for-local-files-using-open-source-models-b09af871751c
+
