beb8c33b4511becdb932485dbf9ca8fb4769b5c1
moljac <mcvjetko@holisticware.net>
2026-02-02 22:30:03 +0100

    exception handling pdfminer

--- samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/text-from-documents/pdf/pdfminer.six/api_text_extraction_pdfminer_six.py
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/text-from-documents/pdf/pdfminer.six/api_text_extraction_pdfminer_six.py
@@ -19,10 +19,22 @@ def extract_text_to_file_from_pdf_document (source: str) -> str:
     time_start_3 = perf_counter_ns()
     #---------------------------------------------------------------------------
 
-    result_txt = extract_text(source)
-
     directory = f"{source}.hwaifs/text/py/pdfminer_six/"
     Path(directory).mkdir(parents=True, exist_ok=True)
+
+    try:
+        result_txt = extract_text(source)        
+    except Exception as e:
+        tb = traceback.format_exc()
+        msg = \
+            f"Exception reading tables from PDF document source = {source} : {e}" \
+            + \
+            tb
+        timestamp = datetime.datetime.now().isoformat().replace(":", "-")
+        with open(f"{directory}/exception-{timestamp}.py.json", "w") as f:
+            f.write(msg)
+        
+        return
 
     # save to file
     with open(f"{directory}/content.txt", "w") as f:
