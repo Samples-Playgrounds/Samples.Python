020fd81d662c1b8c765dbd72fe89d74bf3f4ef2b
moljac <mcvjetko@holisticware.net>
2026-02-23 22:44:37 +0100

    commonmark

---
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/transformations/markdown-parsing/commonmark-py/main.py
@@ -0,0 +1,57 @@
+"""
+deactivate
+rm -fr .venv/ __pycache__/
+rm *.pyc
+"""
+
+"""
+python -m venv .venv
+source .venv/bin/activate
+pip install commonmark
+pip freeze > requirements.txt
+"""
+
+"""
+pip install -r requirements.txt
+python main.py
+"""
+
+import api_parse_analyze_markdown_commonmark as api
+
+
+import sys  
+import os
+scriptpath = "../../../../"
+
+# Add the directory containing your module to the Python path (wants absolute paths)
+sys.path.append(os.path.abspath(scriptpath))
+from data import *
+
+root="../../../../../data/"
+
+sources = files_documents_pdfs
+
+def main():
+   for source in files_documents_pdfs:
+      source = source.replace(
+                                 "../../../../../../../data/", 
+                                 "../../../../../../data/"
+                              )
+      print(120 * "=")
+      print(f"commonmark <- source = {source}")
+      for file in [
+                     "text/py/pymupdf4llm/content.md",
+                     "text/py/docling/content.md",
+                     "text/py/docling/content.filetype-agnostic.md",
+                     "text/py/marker/content.md",
+                     "text/py/MarkItDown/content.txt",
+                  ]:
+         print(100 * ".")
+         file = f"{source}.hwaifs/{file}"
+         print(f"                = {file}")
+         paragraphs = api.api_parse_analyze_markdown_commonmark(file)
+         print(f"commonmark <- paragraphs = {paragraphs}")
+
+
+if __name__ == '__main__':
+    main()
--- samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/transformations/markdown-parsing/commonmark-py/readme.md
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/transformations/markdown-parsing/commonmark-py/readme.md
@@ -20,6 +20,23 @@ print(html) # <p>Hello <em>World</em><p/>
 # inspecting the abstract syntax tree
 json = commonmark.dumpJSON(ast)
 commonmark.dumpAST(ast) # pretty print generated AST structure
+
+
+```
+
+```python
+import commonmark 
+parser = commonmark.Parser() 
+ast = parser.parse(p.content)
+
+paragraphs = '' for node in ast.walker():
+    if node[0].t == "paragraph":
+        paragraphs += " "
+        paragraphs += node[0].first_child.literal
+
 ```
 
 
+*   https://dev.to/waylonwalker/using-a-python-markdown-ast-to-find-all-paragraphs-2876
+
+*   https://waylonwalker.com/python-markdown-ast-paragraph/
