42ce0c76cd4faefa36b7c2fa19a083e2b45b4646
moljac <mcvjetko@holisticware.net>
2026-02-23 22:45:04 +0100

    Update main.py

--- samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/transformations/markdown-parsing/markdown-analysis/main.py
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/transformations/markdown-parsing/markdown-analysis/main.py
@@ -37,17 +37,20 @@ def main():
                                  "../../../../../../../data/", 
                                  "../../../../../../data/"
                               )
-      #print(f"mrkdwn_analysis <- source = {source}")
-      for p in [
-                  "text/py/pymupdf4llm/content.md",
-                  "text/py/markitdown/content.md",
-                  "text/py/docling/content.md",
-                  "text/py/docling/content.filetype-agnostic.md",
-                  "text/py/marker/content.md"
-               ]:
-         file = f"{source}.hwaifs/{p}"
-         print(f"mrkdwn_analysis <- source = {file}")
-         api.api_parse_analyze_markdown_mrkdwn_analysis(file)
+      print(120 * "=")
+      print(f"markdown-analysis <- source = {source}")
+      for file in [
+                     "text/py/pymupdf4llm/content.md",
+                     "text/py/docling/content.md",
+                     "text/py/docling/content.filetype-agnostic.md",
+                     "text/py/marker/content.md",
+                     "text/py/MarkItDown/content.txt",
+                  ]:
+         print(100 * ".")
+         file = f"{source}.hwaifs/{file}"
+         print(f"                = {file}")
+         paragraphs = api.api_parse_analyze_markdown_commonmark(file)
+         print(f"markdown-analysis <- paragraphs = {paragraphs}")
 
 
 if __name__ == '__main__':
