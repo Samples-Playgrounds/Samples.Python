69d52aa3cb09bfe29dffc61f6542c79c9dfb4aa9
moljac <mcvjetko@holisticware.net>
2026-03-01 22:39:03 +0100

    Create test.sh

---
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/transformations/markdown-parsing/mistletoe/test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+LIBRARY=mistletoe
+figlet $LIBRARY
+figlet start
+
+pwd
+
+deactivate
+rm -fr .venv/ __pycache__/
+rm *.pyc
+
+
+python -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip
+
+pip install mistletoe
+
+
+pip install timer
+pip install codetiming
+
+pip freeze > requirements.txt
+
+
+python main.py
+
+pwd
+figlet stop
+figlet $LIBRARY
