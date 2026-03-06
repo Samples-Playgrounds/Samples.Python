17563493c7b2ff3a768129e7fe10ec6d7edef31d
moljac <mcvjetko@holisticware.net>
2026-03-06 16:36:46 +0100

    data refactoring

---
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/data_libraries.py
@@ -0,0 +1,36 @@
+import 
+
+
+#-----------------------------------------------------------------------------------------------------------------------
+# libraries
+libs_text_extraction_from_documents_pdf = [
+                                                "PyMuPDF",
+                                                "PyPDF2",
+                                                "docling",
+                                                "docTR",
+                                                "kreuzberg",
+                                                "marker",
+                                                "MarkItDown",
+                                                "pdfium2",
+                                                "pdfminer.six",
+                                                "pdfplumber",
+                                                "pymupdf4llm-c",
+                                                "pymupdf4llm",
+                                                "pytesseract",
+                                                "unstructured",
+                                            ]
+
+libs_text_extraction_from_documents_ms_word_docx_doc = [
+                                                            "python-docx",
+                                                            "docx2python",
+                                                            "docx2txt",
+                                                            "docxpy",
+                                                            "kreuzberg",
+                                                            "Simplify-Docx"
+                                                        ]
+
+libs_text_extraction_from_documents_ms_powerpoint_pptx = [
+                                                            "python-pptx",
+                                                            "pptx2python",
+                                                            "pptx2txt",
+                                                        ]                                        
