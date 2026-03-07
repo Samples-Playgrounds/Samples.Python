c37921eea9a44eb6d3fe2a63a413b8d4aa57a5dc
moljac <mcvjetko@holisticware.net>
2026-03-07 09:09:08 +0100

    Update api_tts_text_to_speech_pyt2s.py

--- samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/transformations/tts-text-to-speech/pyt2s/api_tts_text_to_speech_pyt2s.py
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/transformations/tts-text-to-speech/pyt2s/api_tts_text_to_speech_pyt2s.py
@@ -18,11 +18,7 @@ def speak (text: str, file: = None str ) -> str:
     time_start_3 = perf_counter_ns()
     #---------------------------------------------------------------------------
 
-    tts = gTTS(text)
 
-    # if not file:
-    if file:
-        tts.save(file)
     #---------------------------------------------------------------------------
     time_stop_1 = time.time()
     time_total_1 = time_stop_1 - time_start_1
@@ -32,7 +28,7 @@ def speak (text: str, file: = None str ) -> str:
     time_total_3 = (time_stop_3 - time_start_3) / 1_000_000_000
 
     times = {
-        "function_method_name" : "extract_text_to_file_from_any_document",
+        "function_method_name" : "speak",
         "time_start_1": time_start_1,
         "time_end_1": time_stop_1,
         "time_total_1": time_total_1,
