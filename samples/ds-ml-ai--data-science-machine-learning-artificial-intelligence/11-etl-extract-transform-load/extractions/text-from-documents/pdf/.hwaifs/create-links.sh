fb5cf69f1e96a6715e09cd81ad48d414fa2925bd
moljac <mcvjetko@holisticware.net>
2026-03-07 09:08:04 +0100

    hwaifs

---
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/text-from-documents/pdf/.hwaifs/create-links.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export DIR_PREFIX=./ai/models/
+
+ln -s \
+    "~/.lmstudio/models/unsloth/Qwen3.5-35B-A3B-Experiments-GGUF/Qwen3.5-35B-A3B_tok-16_out-16_exp-16-16-16_shr-16-16-16_ssm-16-16-16-32_atn-16-16-16-16-16-16.gguf" \
+    $DIR_PREFIX/Qwen3.5-35B-A3B
+
+ln -s \
+    "~/.lmstudio/models/unsloth/Qwen3-Coder-30B-A3B-Instruct-GGUF/Qwen3-Coder-30B-A3B-Instruct-BF16-00001-of-00002.gguf" \
+    $DIR_PREFIX/Qwen3-Coder-30B-A3B-Instruct-BF16
+
+ln -s \
+    "~/.lmstudio/models/unsloth/Qwen3-Coder-Next-GGUF/Qwen3-Coder-Next-UD-Q8_K_XL-00001-of-00003.gguf" \
+    $DIR_PREFIX/Qwen3-Coder-Next-UD-Q8_K_XL
+
+ln -s \
+    "~/.lmstudio/models/unsloth/Qwen3.5-122B-A10B-GGUF/Qwen3.5-122B-A10B-Q5_K_M-00001-of-00003.gguf" \
+    $DIR_PREFIX/Qwen3.5-122B-A10B-Q5_K_M
+    
+ln -s \
+    "~/.lmstudio/models/unsloth/Qwen3.5-35B-A3B-GGUF/Qwen3.5-35B-A3B-UD-Q8_K_XL.gguf" \
+    $DIR_PREFIX/Qwen3.5-35B-A3B-UD-Q8_K_XL
