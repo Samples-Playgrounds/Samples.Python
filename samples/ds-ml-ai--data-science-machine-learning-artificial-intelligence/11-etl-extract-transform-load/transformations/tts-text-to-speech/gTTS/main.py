59da2fe796cd06e6945a694ac21339fd29b080e4
moljac <mcvjetko@holisticware.net>
2026-03-07 09:09:02 +0100

    gTTS

--- samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/transformations/tts-text-to-speech/gTTS/main.py
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/transformations/tts-text-to-speech/gTTS/main.py
@@ -20,18 +20,56 @@ python main.py
 
 import api_tts_text_to_speech_gTTS as api
 
+import sys
+import os
+scriptpath = "../../../../"
 
-# document per local path or URL
-sources = [
-   "Hi Moljac. What's up?",
-]
+# Add the directory containing your module to the Python path (wants absolute paths)
+sys.path.append(os.path.abspath(scriptpath))
+from data import *
+
+
+texts_simple = [
+                     "Hi Moljac. What's up?",
+                  ]
+
 
 def main():
-   for source in sources:
-      print(f"pyttsx3 <- source = {source}")
-      api.speak(source)
+
+   for text in texts_simple:
+
+      print(f"gTTS <- text = {text}")
+      file_audio = api.speak(text)
+
+      os.startfile(file_audio)
+
+
+   for source in files_documents_pdfs:
+      source = source.replace(
+                                 "../../../../../../../data/", 
+                                 "../../../../../../data/"
+                              )
+      print(120 * "=")
+      print(f"gTTS <- source = {source}")
+      for file in [
+                     "text/py/pymupdf4llm/content.md",
+                     "text/py/docling/content.md",
+                     "text/py/docling/content.filetype-agnostic.md",
+                     "text/py/marker/content.md",
+                     "text/py/MarkItDown/content.txt",
+                  ]:
+         print(100 * ".")
+         file = f"{source}.hwaifs/{file}"
+         print(f"                = {file}")
+
+         with open(file, 'r') as f:
+            text = f.read()
+      
+         api.speak(text, "en")
+
 
 
 if __name__ == '__main__':
     main()
+
 
