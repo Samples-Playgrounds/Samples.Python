237649584295da41fdd2f82e5f31d4fe4ccc1a18
moljac <mcvjetko@holisticware.net>
2026-03-17 22:04:41 +0100

    Update api_text_extraction_from_images_keras_ocr.py

--- samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/text-from-images/z_keras-ocr/api_text_extraction_from_images_keras_ocr.py
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/text-from-images/z_keras-ocr/api_text_extraction_from_images_keras_ocr.py
@@ -10,40 +10,55 @@ from time import perf_counter
 from time import perf_counter_ns
 # from timer import timer
 
+library_name = "keras-ocr"
+
 #@timer()
-def extract_text_to_file_from_image (source: str) -> str:
+def extract_text_to_file_from_image (
+                                        source: str
+                                    ) -> str:
 
     #---------------------------------------------------------------------------
     time_start_1 = time.time()
     time_start_2 = perf_counter()
     time_start_3 = perf_counter_ns()
     #---------------------------------------------------------------------------
-
-    directory = f"{source}.hwaifs/text/py/keras-ocr/"
+    directory = f"{source_file}.hwaifs/extractions/text/py/{library_name}/"
     Path(directory).mkdir(parents=True, exist_ok=True)
 
+    try:
+        # keras-ocr will automatically download pretrained
+        # weights for the detector and recognizer.
+        pipeline = keras_ocr.pipeline.Pipeline()
 
-    # keras-ocr will automatically download pretrained
-    # weights for the detector and recognizer.
-    pipeline = keras_ocr.pipeline.Pipeline()
+        # Each list of predictions in prediction_groups is a list of
+        # (word, box) tuples.
+        prediction_groups = pipeline.recognize(images)
 
-    # Each list of predictions in prediction_groups is a list of
-    # (word, box) tuples.
-    prediction_groups = pipeline.recognize(images)
+        # Plot the predictions
+        fig, axs = plt.subplots(nrows=len(images), figsize=(20, 20))
+        for ax, image, predictions in zip(axs, images, prediction_groups):
+            keras_ocr.tools.drawAnnotations(image=image, predictions=predictions, ax=ax)
 
-    # Plot the predictions
-    fig, axs = plt.subplots(nrows=len(images), figsize=(20, 20))
-    for ax, image, predictions in zip(axs, images, prediction_groups):
-        keras_ocr.tools.drawAnnotations(image=image, predictions=predictions, ax=ax)
+        text_complete = ""
 
-    text_complete = ""
+        for (bbox, text, prob) in result:
+            text_complete += text + '\n'
 
-    for (bbox, text, prob) in result:
-        text_complete += text + '\n'
+        # save to file
+        with open(f"{directory}/content.txt", "w") as f:
+            f.write(text_complete) 
 
-    # save to file
-    with open(f"{directory}/content.txt", "w") as f:
-        f.write(text_complete) 
+    except Exception as e:
+        tb = traceback.format_exc()
+        msg = \
+            f"Exception reading text with {library_name} from image = {source_file} : {e}" \
+            + \
+            tb
+        timestamp = datetime.datetime.now().isoformat().replace(":", "-")
+        with open(f"{directory}/exception-{timestamp}.py.json", "w") as f:
+            f.write(msg)
+        
+        return
 
     #---------------------------------------------------------------------------
     time_stop_1 = time.time()
