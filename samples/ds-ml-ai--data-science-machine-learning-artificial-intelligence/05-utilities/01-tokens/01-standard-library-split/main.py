fd84b28ce4ecf95b072a183040d60b4966f8f70a
moljac <mcvjetko@holisticware.net>
2026-03-18 21:25:11 +0100

    Create main.py

---
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/05-utilities/01-tokens/01-standard-library-split/main.py
@@ -0,0 +1,81 @@
+"""
+deactivate
+rm -fr .venv/ __pycache__/
+rm *.pyc
+"""
+
+"""
+python -m venv .venv
+source .venv/bin/activate
+
+pip install orjson
+pip install timer
+pip install codetiming
+pip freeze > requirements.txt
+"""
+
+"""
+pip install -r requirements.txt
+python main.py
+"""
+
+texts = [
+            """
+            """,
+            "",
+            "Large string .....",
+            "word",
+            "some sentence",
+            "tiktoken is great!",
+        ]
+
+for text in texts:
+    print(120 * "=")
+    words = text.split()      
+    print(words)
+    print(100 * ".")
+    words = text.split(",")   
+    print(words)
+    print(100 * ".")
+    print(list(text))
+    print(100 * ".")
+    count_characters = len(text) // 4
+    print(f"count characters: {count_characters}")
+    count_tokens = count_characters // 4
+    print(f"count tokens: {count_tokens}")
+
+import api_tokens_stdlib_split as api
+
+import sys
+import os
+scriptpath = "../../../"
+
+# Add the directory containing your module to the Python path (wants absolute paths)
+sys.path.append(os.path.abspath(scriptpath))
+from data import *
+
+
+def main():
+
+    for text in texts:
+        print(120 * "=")
+        result_1 = api.tokens_characters_in_text(text)
+        print(f"characters: {result_1[0]}")
+        print(f"         count characters   : {result_1[1]}")
+        print(f"         count tokens       : {result_1[2]}")
+        result_2 = api.tokens_words_in_text(text)
+        print(f"words: {result_2[0]}")
+        print(f"         count words        : {result_2[1]}")
+        print(f"         count tokens min   : {result_2[2]}")
+        print(f"         count tokens max   : {result_2[3]}")
+
+   
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
