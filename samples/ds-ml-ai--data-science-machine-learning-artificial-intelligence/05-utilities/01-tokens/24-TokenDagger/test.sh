a17c4d67dd165527bf5bf069a1accb51c250c1ac
moljac <mcvjetko@holisticware.net>
2026-03-19 00:10:32 +0100

    Create test.sh

---
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/05-utilities/01-tokens/24-TokenDagger/test.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+LIBRARY=24-TokenDagger
+figlet $LIBRARY
+figlet start
+
+pwd
+
+deactivate
+rm -fr .venv/ __pycache__/
+rm *.pyc
+
+
+python -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip
+
+pip install tokendagger
+
+pip install orjson
+pip install timer
+pip install codetiming
+
+pip freeze > requirements.txt
+
+
+python main.py
+
+pwd
+
+figlet stop
+figlet $LIBRARY
+
+cd ../
+pwd
