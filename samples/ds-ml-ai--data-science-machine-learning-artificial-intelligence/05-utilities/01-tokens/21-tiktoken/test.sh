80e1de2a30444a6bb1c8085f719dbb3af23de701
moljac <mcvjetko@holisticware.net>
2026-03-18 21:25:49 +0100

    Create test.sh

---
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/05-utilities/01-tokens/21-tiktoken/test.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+LIBRARY=21-tiktoken
+figlet $LIBRARY
+figlet start
+
+pwd
+
+deactivate
+rm -fr .venv/ __pycache__/
+rm *.pyc
+
+
+python -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip
+
+pip install tiktoken
+
+pip install orjson
+pip install timer
+pip install codetiming
+
+pip freeze > requirements.txt
+
+
+python main.py
+
+pwd
+
+figlet stop
+figlet $LIBRARY
+
+cd ../
+pwd
