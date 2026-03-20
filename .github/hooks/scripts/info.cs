db34ce1e97f5ac3d2463569097a9f647674fb8e2
moljac <mcvjetko@holisticware.net>
2026-03-20 15:21:34 +0100

    Create info.cs

---
+++ .github/hooks/scripts/info.cs
@@ -0,0 +1,6 @@
+#!/usr/bin/env dotnet
+
+/*
+https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/tutorials/file-based-programs
+*/
+Console.WriteLine("This is the info.cs file, which contains information about the hooks and their usage.");
