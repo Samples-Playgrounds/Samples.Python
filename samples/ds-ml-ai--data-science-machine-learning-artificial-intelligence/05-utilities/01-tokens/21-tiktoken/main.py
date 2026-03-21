b1238b63382a774ebf3f18c63dc27a984682d8e7
moljac <mcvjetko@holisticware.net>
2026-03-21 23:35:20 +0100

    Update main.py

--- samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/05-utilities/01-tokens/21-tiktoken/main.py
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/05-utilities/01-tokens/21-tiktoken/main.py
@@ -27,6 +27,17 @@ texts = [
             "word",
             "some sentence",
             "tiktoken is great!",
+            "This is an example sentence, showing off the tokenization process.",
+            """
+            In the face of changing market conditions and pressure to accelerate growth, the ability for customers to
+            do more with less is critical. And while the cloud has long been the north star for realizing efficiencies
+            and accelerating innovation, at Microsoft, we understand that these benefits also need to happen outside
+            of the cloud. In 2021 we announced the general availability of Azure Arc-enabled SQL Managed Instance.
+            Previously only available in Azure, Azure Arc-enabled SQL Managed Instance allows customers to build new
+            cloud native applications on any infrastructure, in their on-premises environments and across other public
+            clouds. Now, we are offering a way for customers to make the most of their legacy applications, with Azure
+            Arc-enabled SQL Server.
+            """,
         ]
 
 
