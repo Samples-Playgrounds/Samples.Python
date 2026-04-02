77f16033a7765a4423cd4899c4c9adc2793c2370
moljac <mcvjetko@holisticware.net>
2026-04-02 21:30:43 +0200

    Update main.py

--- samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/text-from-documents/pdf/unstructured/main.py
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/text-from-documents/pdf/unstructured/main.py
@@ -30,6 +30,10 @@ sys.path.append(os.path.abspath(scriptpath))
 from data import *
 
 def main():
+   for source in files_documents_pdfs:
+      if source.endswith(".pdf"):
+         print(f"unstructured <- source :  {source}")
+         result_txt = api.extract_text_to_file_from_pdf_document(source)
 
    for source in sources:
       if source.endswith(".pdf"):
