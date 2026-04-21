d2346661354b994e73e2b6e77fd7a22239d1d175
moljac <mcvjetko@holisticware.net>
2026-04-21 22:16:11 +0200

    Create main.py

---
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/text-from-documents/microsoft-word-docx/docx2md/main.py
@@ -0,0 +1,41 @@
+"""
+deactivate
+rm -fr .venv/ __pycache__/
+rm *.pyc
+"""
+
+"""
+python -m venv .venv
+source .venv/bin/activate
+pip install docx2md
+pip install orjson
+pip install timer
+pip install codetiming
+pip freeze > requirements.txt
+"""
+
+"""
+pip install -r requirements.txt
+python main.py
+"""
+
+import docx2md.api_text_extraction_docx2md as api
+
+import sys
+import os
+scriptpath = "../../../../../"
+
+# Add the directory containing your module to the Python path (wants absolute paths)
+sys.path.append(os.path.abspath(scriptpath))
+from data import *
+
+sources = files_documents_docxs
+
+def main():
+   for source in sources:
+      if source.endswith(".docx"):
+         print(f"docx2python <- source = {source}")
+         result_txt = api.extract_text_to_file_from_docx_document(source)
+
+if __name__ == '__main__':
+    main()
