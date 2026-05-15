d1819562b72358e05e36c69fa3602fa385a6f683
moljac <mcvjetko@holisticware.net>
2026-05-15 10:16:25 +0200

    crawl4ai initial commit

---
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/42-search/crawl4ai/main.py
@@ -0,0 +1,51 @@
+"""
+deactivate
+rm -fr .venv/ __pycache__/
+rm *.pyc
+"""
+
+"""
+python -m venv .venv
+source .venv/bin/activate
+
+pip install crawl4ai
+# Setup the browser
+crawl4ai-setup
+
+pip install orjson
+pip install timer
+pip install codetiming
+pip freeze > requirements.txt
+"""
+
+"""
+pip install -r requirements.txt
+python main.py
+"""
+
+
+import asyncio
+from crawl4ai import *
+
+
+import api_search as api
+
+import sys
+import os
+scriptpath = "../../"
+
+# Add the directory containing your module to the Python path (wants absolute paths)
+sys.path.append(os.path.abspath(scriptpath))
+from data import *
+
+
+async def main():
+    site_url = 'https://aspire.dev/docs/'
+    async with AsyncWebCrawler() as crawler:
+        result = await crawler.arun(
+                                        url=site_url
+                                    )
+        print(result.markdown)
+
+if __name__ == '__main__':
+    asyncio.run(main())
--- samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/42-search/crawl4ai/readme.md
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/42-search/crawl4ai/readme.md
@@ -2,8 +2,26 @@
 
 *   https://github.com/unclecode/crawl4ai
 
-```
+*   https://pypi.org/project/Crawl4AI/
+
+```shell
 pip install crawl4ai
 # Setup the browser
 crawl4ai-setup
+```
+
+```python
+import asyncio
+from crawl4ai import *
+
+async def main():
+    str site = "https://aspire.dev/docs/"
+    async with AsyncWebCrawler() as crawler:
+        result = await crawler.arun(
+                                        url=site
+                                    )
+        print(result.markdown)
+
+if __name__ == "__main__":
+    asyncio.run(main())
 ```
