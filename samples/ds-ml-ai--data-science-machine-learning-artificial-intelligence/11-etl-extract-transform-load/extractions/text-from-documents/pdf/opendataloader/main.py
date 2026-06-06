d70ba8de1a8ee33f266751c401cb1256fc71efc0
moljac <mcvjetko@holisticware.net>
2026-06-06 20:08:37 +0200

    Create main.py

---
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/11-etl-extract-transform-load/extractions/text-from-documents/pdf/opendataloader/main.py
@@ -0,0 +1,47 @@
+"""
+deactivate
+rm -fr .venv/ __pycache__/
+rm *.pyc
+"""
+
+"""
+python -m venv .venv
+source .venv/bin/activate
+pip install opendataloader-pdf
+pip install orjson
+pip install timer
+pip install codetiming
+pip freeze > requirements.txt
+"""
+
+"""
+pip install -r requirements.txt
+python main.py
+"""
+
+import api_text_extraction_opendataloader as api
+
+import sys
+import os
+scriptpath = "../../../../../"
+
+# Add the directory containing your module to the Python path (wants absolute paths)
+sys.path.append(os.path.abspath(scriptpath))
+from data import *
+
+
+def main():
+   for source in files_documents_pdfs:
+      print(f"MarkItDown <- source = {source}")
+      result_txt = api.extract_text_to_file_from_pdf_document(source)
+
+   for source in sources:
+      print(f"MarkItDown <- source = {source}")
+      result_txt = api.extract_text_to_file_from_pdf_document(source)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
