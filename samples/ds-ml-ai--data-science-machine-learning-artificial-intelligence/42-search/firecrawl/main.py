490cc01f39f6e81d82c5a7399faca31af4e0e5ea
moljac <mcvjetko@holisticware.net>
2026-07-14 21:04:23 +0200

    Create main.py

---
+++ samples/ds-ml-ai--data-science-machine-learning-artificial-intelligence/42-search/firecrawl/main.py
@@ -0,0 +1,76 @@
+"""
+deactivate
+rm -fr .venv/ __pycache__/
+rm *.pyc
+"""
+
+"""
+python -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip
+
+pip install social-analyzer
+
+pip install orjson
+pip install timer
+pip install codetiming
+pip freeze > requirements.txt
+"""
+
+"""
+pip install -r requirements.txt
+python main.py
+"""
+
+
+from importlib import import_module
+SocialAnalyzer = import_module("social-analyzer").SocialAnalyzer(silent=True)
+results = SocialAnalyzer.run_as_object(
+                                            username="moljac",
+                                            silent=True
+                                        )
+print(results)
+
+
+
+
+
+
+
+
+
+
+
+# import api_search as api
+# 
+# import sys
+# import os
+# scriptpath = "../../"
+# 
+# # Add the directory containing your module to the Python path (wants absolute paths)
+# sys.path.append(os.path.abspath(scriptpath))
+# from data import *
+# 
+# def main():
+#     sites_urls = [
+#                     "https://aspire.dev/docs/",
+#                     "https://learn.microsoft.com/en-us/dotnet/maui/",
+#                     "https://learn.microsoft.com/en-us/dotnet/",
+#                     "https://learn.microsoft.com/en-us/aspnet/core/",
+#                     "https://learn.microsoft.com/en-us/azure/",
+#                     "https://learn.microsoft.com/en-us/dotnet/csharp/",
+#                  ]
+# 
+# # async def main():
+# #     site_url = 'https://aspire.dev/docs/'
+# #     async with AsyncWebCrawler() as crawler:
+# #         result = await crawler.arun(
+# #                                         url=site_url
+# #                                     )
+# #         print(result.markdown)
+# 
+# if __name__ == '__main__':
+#     
+#     main()
+#     # asyncio.run(main())
+# 
